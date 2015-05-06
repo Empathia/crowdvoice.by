38c09de868969656f97e6313d1b1ffdad0def61b
Pablo Gonzalez <pbglezc@gmail.com>
2015-05-06 13:19:32 -0500

    Add more validations for Users

--- models/User.js
+++ models/User.js
@@ -3,6 +3,17 @@ var bcrypt = require('bcrypt-nodejs');
 var User = Class('User').inherits(Argon.KnexModel)({
 
   validations : {
+    username: [
+      'required',
+      { 
+        rule: function (val) {
+          return db('Users').where('username', '=', val).then(function(resp) {
+            if (resp.length > 0) throw new Error('This username is already in use.')
+          });
+        },
+        message: 'This username is already in use.'
+      }
+    ],
     email: [
       'email', 'required',
       { 
@@ -13,6 +24,9 @@ var User = Class('User').inherits(Argon.KnexModel)({
         },
         message: 'This email address is already in use.'
       }
+    ],
+    password: [
+      'minLength:8'
     ]
   },
 
@@ -28,7 +42,10 @@ var User = Class('User').inherits(Argon.KnexModel)({
 User.bind('beforeSave', function (event) {
   var model = event.data.model;
 
-  if (model.password) {
+  /* Here the best case scenario is to verify that the model is valid by executing isValid, but since 
+   * we cannot use it because because Argon does not trigger 'beforeSave' asynchronously,
+   * then we have to replicate the validation: password.length >= 8 */
+  if (model.password && model.password.length >= 8) {
     model.encryptedPassword = bcrypt.hashSync(model.password, bcrypt.genSaltSync(10), null);
     delete model.password;
   }
