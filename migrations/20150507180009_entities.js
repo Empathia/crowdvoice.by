c73f225aec04bbe87f202d6f578bee2b0a78892e
Pablo Gonzalez <pbglezc@gmail.com>
2015-05-07 18:16:16 -0500

    Add Entities migration

---
+++ migrations/20150507180009_entities.js
@@ -0,0 +1,22 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('Entities', function (t) {
+      t.increments('id').primary();
+
+      t.string('type', 32).defaultTo("person");
+      t.string('name', 512).defaultTo(null);
+      t.string('lastname', 512).defaultTo(null);
+      t.boolean('isAnonymous').defaultTo(false);
+
+      t.timestamps();
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('Entities')
+  ]);
+};
