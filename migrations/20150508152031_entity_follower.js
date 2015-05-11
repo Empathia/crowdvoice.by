cfb5a6ee9ce45766ac8be7f3e5543a96aaec6200
Pablo Gonzalez <pbglezc@gmail.com>
2015-05-11 10:33:09 -0500

    Add EntityFollower model

--- migrations/20150508152031_entity_follower.js
+++ migrations/20150508152031_entity_follower.js
@@ -3,8 +3,12 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('EntityFollower', function (t) {
+      t.increments('id').primary();
+
       t.integer('follower_id').defaultTo(0);
       t.integer('followed_id').defaultTo(0);
+
+      t.timestamps();
     })
   ]);
 };
