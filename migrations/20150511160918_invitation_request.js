a588d53dfa0f64a2fc710ccabd09e56be5a9ded0
Pablo Gonzalez <pbglezc@gmail.com>
2015-05-11 16:14:50 -0500

    Add migration for InvitationRequest

---
+++ migrations/20150511160918_invitation_request.js
@@ -0,0 +1,20 @@
+'use strict';
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('InvitationRequest', function (t) {
+      t.increments('id').primary();
+
+      t.integer('invitator_entity_id').defaultTo(0);
+      t.integer('invited_entity_id').defaultTo(0);
+
+      t.timestamps();
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('VoiceFollowers')
+  ]);
+};
