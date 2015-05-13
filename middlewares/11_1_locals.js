6627dd28ab7891fb4d4f219e58bf72c85abafd33
Pablo Gonzalez <pbglezc@gmail.com>
2015-05-13 17:09:43 -0500

    Add follow to OrganizationsController

--- middlewares/11_1_locals.js
+++ middlewares/11_1_locals.js
@@ -26,5 +26,17 @@ module.exports = function(req, res, next) {
     res.locals.hashids = global.hashids;
   }
 
-  next();
+  // Add currentEntity
+  if (req.user) {
+    var currentUser = new User(req.user);
+    currentUser.entity(function (err, entity) {
+      res.locals.currentEntity = entity;
+
+      next();
+    });
+  } else {
+    res.locals.currentEntity = null;
+    next();
+  }
+
 }
