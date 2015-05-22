b387b0b574cc742a5dd698ef6ead1987d42c1a03
Pablo Gonzalez <pbglezc@gmail.com>
2015-05-22 10:35:51 -0500

    Implement index, show and create in PersonsController.

--- controllers/PersonsController.js
+++ controllers/PersonsController.js
@@ -1,8 +1,35 @@
 var PersonsController = Class('PersonsController').inherits(RestfulController)({
   prototype : {
-    // _initRouter : function() {
-    //   RestfulController.prototype._initRouter.apply(this, arguments);
-    // },
+    _initRouter : function() {
+      application.router.route('/persons').get(this.index);
+      application.router.route('/person').post(this.create);
+      application.router.route('/person/new').get(this.new);
+
+      application.router.route('/person/:id*').all(this.getPerson);
+      application.router.route('/person/:id').get(this.show);
+      application.router.route('/person/:id').put(this.update);
+      application.router.route('/person/:id/edit').get(this.edit);
+
+      // application.router.route('/person/:id/follow').post(this.follow);
+      // application.router.route('/person/:id/voices').get(this.voices);
+    },
+
+    getPerson : function getPerson (req, res, next) {
+      Entity.find({
+        id: req.params.id,
+        type: 'person'
+      }, function (err, result) {
+        if (err) { next(err); return; }
+        if (result.length === 0) {
+          res.status(404);
+          res.render('shared/404.html');
+          return;
+        }
+
+        res.locals.person = new Entity(result[0]);
+        req.person = new Entity(result[0]);
+      });
+    },
 
     index : function index (req, res) {
       Entity.find({type:'person'}, function (err, result) {
@@ -23,12 +50,43 @@ var PersonsController = Class('PersonsController').inherits(RestfulController)({
       res.render('persons/show.html', {layout : false});
     },
 
+    showByProfileName : function showByProfileName (req, res, next) {
+      Entity.find({
+        type: 'person',
+        profile_name: req.params.profile_name
+      }, function (err, result) {
+        if (err) { next(err); return; }
+        if (result.length === 0) {
+          res.status(404);
+          res.render('shared/404.html');
+          return;
+        }
+
+        res.locals.person = new Entity(result[0]);
+        req.person = new Entity(result[0]);
+        res.render('persons/show.html');
+      });
+    },
+
     new : function (req, res) {
       res.render('persons/new.html');
     },
 
     create : function create (req, res) {
-      res.redirect('/person/id');
+      var person = new Entity({
+        name: req.body['name'],
+        lastname: req.body['lastname'],
+        profileName: req.body['profileName'],
+        isAnonymous: req.body['isAnonymous'] === "true" ? true : false
+      });
+      person.type = 'person';
+      person.save(function (err) {
+        if (err) {
+          res.render('organizations/new.html', {errors: err});
+        } else {
+          res.redirect('/' + person.profileName);
+        }
+      });
     },
 
     edit : function edit (req, res) {
