21489f65edba9e3144a843827c6658a78ee5803b
Sergio de la Garza <sergio@delagarza.io>
2015-05-23 17:58:31 -0500

    Finished Tests and ThreadsController

--- test/controllers/ThreadsContrroller.js
+++ test/controllers/ThreadsContrroller.js
@@ -13,7 +13,7 @@ var async = require('async');
 var request = require('superagent');
 var crypto = require('crypto');
 
-CONFIG.database.logQueries = true;
+CONFIG.database.logQueries = false;
 
 var urlBase = 'http://localhost:3000';
 var csrf;
@@ -113,6 +113,140 @@ Tellurium.suite('Threads Controller Suite')(function(){
             spec.assert(res.body.length).toBe(3);
             spec.completed();
           })
+      })
+    });
+
+    desc.specify('Delete a thread should mark it as hidden for the currentPerson')(function(spec) {
+      var jack = suite.registry.jack;
+
+      async.series([
+        function (done) {
+          var req = request
+            .get(urlBase + '/csrf');
+
+          req.end(function (err, res) {
+            cookies = res.headers['set-cookie'];
+            csrf = res.text;
+            done();
+          });
+        },
+        function (done) {
+          var req = request
+            .post(urlBase + '/session')
+            .set('cookie', cookies)
+            .send({_csrf: csrf, username: 'jack', password: '12345678'});
+
+          req.end(function (err, res) {
+            done();
+          });
+        }
+      ], function(err) {
+        if (err) {
+          throw new Error(err);
+        }
+
+        setTimeout(function() {
+          var req = request
+            .del(urlBase + '/' + jack.profileName + '/messages/1')
+            .accept('application/json')
+            .set('cookie', cookies)
+            .send({_csrf: csrf, method : '_delete'});
+
+            req.end(function(err, res) {
+              if (err) {
+                spec.assert(true).toBe(false);
+              }
+
+              spec.assert(res.status).toBe(200);
+
+              var response = res.body;
+
+              spec.assert(response.status).toBe('ok');
+
+              var req = request
+              .get(urlBase + '/' + jack.profileName + '/messages')
+              .accept('application/json')
+              .set('cookie', cookies);
+
+              setTimeout(function() {
+                req.end(function(err, res) {
+                  if (err) {
+                    spec.assert(true).toBe(false);
+                  }
+
+                  spec.assert(res.status).toBe(200);
+                  spec.assert(res.body.length).toBe(2);
+                  spec.completed();
+                })
+              }, 1000)
+            })
+        }, 1000);
+      })
+
+    });
+
+    desc.specify('Pass create a new Thread and initial message')(function(spec) {
+      var jack = suite.registry.jack;
+      var john = suite.registry.john;
+      var peter = suite.registry.peter;
+      var steve = suite.registry.steve;
+
+      var cookie;
+      var _csrf;
+
+      async.series([
+        function (done) {
+          var req = request
+            .get(urlBase + '/csrf');
+
+          req.end(function (err, res) {
+            cookie = res.headers['set-cookie'];
+            _csrf = res.text;
+            done();
+          });
+        },
+        function (done) {
+          var req = request
+            .post(urlBase + '/session')
+            .set('cookie', cookie)
+            .send({_csrf: _csrf, username: 'steve', password: '12345678'});
+
+          req.end(function (err, res) {
+            done();
+          });
+        }
+      ], function(err) {
+        if (err) {
+          throw new Error(err);
+        }
+
+        // setTimeout(function() {
+          var req = request
+            .post(urlBase + '/' + steve.profileName + '/messages')
+            .accept('application/json')
+            .set('cookie', cookie)
+            .send({
+              _csrf: _csrf,
+              senderPersonId : steve.id,
+              senderEntityId : steve.id,
+              receiverEntityId : peter.id,
+              message : 'Hello Peter, this is Steve!'
+            });
+
+            req.end(function(err, res) {
+              if (err) {
+                spec.assert(true).toBe(false);
+              }
+
+              spec.assert(res.status).toBe(200);
+
+              var response = res.body;
+
+              spec.assert(response.messages.length).toBe(1);
+
+              spec.completed()
+            })
+        // }, 5000);
       })
     })
   })
