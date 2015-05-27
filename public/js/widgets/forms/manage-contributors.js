4cdb67db2bfeeee7effb40d406dfd905e83ecc0f
Rasta <rastafario@gmail.com>
2015-05-27 17:55:29 -0500

    added account page

--- public/js/widgets/forms/manage-contributors.js
+++ public/js/widgets/forms/manage-contributors.js
@@ -41,44 +41,15 @@ Class(CV, 'ManageContributors').inherits(Widget)({
 			    title 		: "Write a message"
 			}).render(this.element.find('.placeholder-main'));
 
-			var users = [
-			  {
-			    img : '/img/sample/covers/feat-00.jpg',
-			    name: 'Carl J. Kramer',
-			    user: '@carlKrameR',
-			    location : 'Atlanta, GA',
-			    date : 'Contributing since Feb, 2015',
-			  },
-			  {
-			    img : '/img/sample/covers/feat-00.jpg',
-			    name: 'Roosbelinda Cárdenas',
-			    user: '@Ross006',
-			    location : 'Stockton, CA',
-			    date : 'Contributing since Jul, 2014',
-			  },
-			  {
-			    img : '/img/sample/covers/feat-00.jpg',
-			    name: 'Carl J. Kramer',
-			    user: '@carlKrameR',
-			    location : 'Atlanta, GA',
-			    date : 'Contributing since Feb, 2015',
-			  },
-			  {
-			    img : '/img/sample/covers/feat-00.jpg',
-			    name: 'Roosbelinda Cárdenas',
-			    user: '@Ross006',
-			    location : 'Stockton, CA',
-			    date : 'Contributing since Jul, 2014',
-			  }
-			];
 
 			new CV.UsersList({
-			    type    	: '',
-			    name  		: '',
-			    style 		: '',
-			    hasButton	: true,
-			    users 		: users,
-			    listTitle 	: "The continuos effects of the fukushima disaster",
+			    type    		: '',
+			    name  			: '',
+			    style 			: '',
+			    hasButton		: true,
+			    users 			: users,
+			    listTitle 		: '{count} Contributors of "The continuos effects of the fukushima disaster"',
+			    dateTitle 		: "Contributing Since",
 			}).render(this.element.find('.placeholder-users'));
 
 
