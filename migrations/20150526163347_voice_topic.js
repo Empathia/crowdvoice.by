d5df276e79a12e1a630b4eed9fbdfd0100863605
Sergio de la Garza <sergio@delagarza.io>
2015-05-27 17:19:29 -0500

    Add new posts migration

--- migrations/20150526163347_voice_topic.js
+++ migrations/20150526163347_voice_topic.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.up = function(knex, Promise) {
- return Promise.all([
+  return Promise.all([
     knex.schema.createTable('VoiceTopic', function(t) {
       t.increments('id').primary();
       t.integer('voice_id').unique().defaultTo(null);
@@ -12,7 +12,7 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
- return Promise.all([
+  return Promise.all([
     knex.schema.dropTable('VoiceTopic')
   ]);
 };
---
+++ migrations/20150527170828_create_posts.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('Posts'),
+    knex.schema.createTable('Posts', function(t) {
+      t.increments('id').primary();
+      t.integer('owner_id').index().notNullable();
+      t.integer('voice_id').index().notNullable();
+      t.boolean('approved').index();
+      t.string('image', 1024);
+      t.integer('image_height').defaultTo(0);
+      t.integer('image_width').defaultTo(0);
+      t.string('source_service').notNullable();
+      t.string('source_type').notNullable();
+      t.string('source_url', 1024).defaultTo(null);
+      t.string('title', 512).defaultTo(null);
+      t.text('description').defaultTo(null);
+      t.dateTime('published_at').index();
+      t.timestamps();
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('Posts')
+  ]);
+};
