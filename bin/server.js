a0ba3f32210084069ade7a2b510abec546e6c572
Sergio de la Garza <sergio@delagarza.io>
2015-06-02 15:56:10 -0500

    Add PostsPresenter

--- bin/server.js
+++ bin/server.js
@@ -7,13 +7,21 @@ var posts = require('../public/demo-data/posts')
 
 application._serverStart();
 
+require('./../presenters/PostsPresenter');
+
 io.on('connection', function(socket) {
   logger.log('socket connected')
 
   socket.on('getMonth', function(dateString, up) {
     var dateData = dateString.split('-');
     Post.find(['"Posts".voice_id = ? AND EXTRACT(MONTH FROM "Posts".published_at) = ? AND EXTRACT(YEAR FROM "Posts".published_at) = ? ORDER BY "Posts".published_at DESC', [1, dateData[1], dateData[0]]], function(err, posts) {
-      socket.emit('monthData', posts, dateString, up);
+      PostsPresenter.build(posts, function(err, result) {
+        if (err) {
+          return socket.emit('monthData', {'error': err}, dateString, up);
+        }
+
+        socket.emit('monthData', results, dateString, up);
+      })
     });
   })
 });
---
+++ presenters/PostsPresenter.js
@@ -0,0 +1,28 @@
+Module('PostsPresenter')({
+  build : function build(posts, callback) {
+    async.each(posts, function(post, next) {
+
+      post.id       = hashids(post.id);
+      post.voiceId  = hashids(post.voiceId);
+
+      async.series([function(done) {
+        Entity.findById(post.ownerId, function(err, result) {
+          if (err) {
+            return done(err);
+          }
+
+          post.owner = new Entity(result[0]);
+          post.owner.id = hashids.encode(post.ownerId);
+
+          delete post.ownerId;
+
+          done();
+        })
+      }], function(err) {
+        next(err);
+      })
+    }, function(err) {
+      callback(err, posts);
+    });
+  }
+});
