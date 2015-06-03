6b9ba4a57d5051961c56234e1053605831d5012a
Pablo Gonzalez <pbglezc@gmail.com>
2015-06-03 18:30:52 -0500

    Add complete HtmlScrapper

--- test/modules/cvscrapper_test.js
+++ test/modules/cvscrapper_test.js
@@ -1,5 +1,5 @@
 var scrapper = require(__dirname + '/../../lib/cvscrapper');
 
-scrapper.processUrl('http://google.com', function (err, info) {
+scrapper.processUrl('http://www.informador.com.mx/jalisco/2015/595796/6/la-ssj-lista-ante-posible-llegada-del-huracan-blanca.htm', function (err, info) {
   console.log(arguments);
 });
