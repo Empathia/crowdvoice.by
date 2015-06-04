f5f2428fe5ddb88b4fc713d136518ff4eb5154d8
Pablo Gonzalez <pbglezc@gmail.com>
2015-06-04 11:06:42 -0500

    Add argon to scrappers and downloaders.

--- test/modules/cvscrapper_test.js
+++ test/modules/cvscrapper_test.js
@@ -1,5 +1,10 @@
+require('argonjs');
+
 var scrapper = require(__dirname + '/../../lib/cvscrapper');
 
-scrapper.processUrl('http://www.informador.com.mx/jalisco/2015/595796/6/la-ssj-lista-ante-posible-llegada-del-huracan-blanca.htm', function (err, info) {
+scrapper.processUrl('http://img.informador.com.mx/biblioteca/imagen/677x508/1179/1178985.jpg', function (err, info) {
   console.log(arguments);
 });
+// scrapper.processUrl('http://www.informador.com.mx/jalisco/2015/595796/6/la-ssj-lista-ante-posible-llegada-del-huracan-blanca.htm', function (err, info) {
+//   console.log(arguments);
+// });
