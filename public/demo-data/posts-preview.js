2e77a425c4d572867719b8325145c31eb7917fe2
Noel Delgado <auhcsei@gmail.com>
2015-06-09 15:10:44 -0500

    add / post preview sample data

---
+++ public/demo-data/posts-preview.js
@@ -0,0 +1,183 @@
+module.exports = [
+    {
+        sourceService: "link",
+        sourceType: "link",
+        sourceUrl: "http://codepen.io/",
+        title: "CodePen - Front End Developer Playground &amp; Code Editor in the Browser",
+        description: "CodePen is for Web Designers and Front End Developers.\n\nCSS tabs and accordions done well\n\n\n\nBash Aliases and CLI Profiles\n",
+        images: [
+            {
+                format: "png",
+                height: 200,
+                path: "/posts_images/downloaded_image_8d848ac0f514abde792ad2eb8ddfd250.png",
+                size: 6503,
+                url: "http://assets.codepen.io/assets/social/facebook-default.png",
+                width: 200
+            },
+            {
+                format: "jpeg",
+                height: 80,
+                path: "/posts_images/downloaded_image_6ca4fe24156bd91a4e0c73a1499b5ed2.jpg",
+                size: 2489,
+                url: "http://s3-us-west-2.amazonaws.com/s.cdpn.io/27167/profile/profile-80_2.jpg",
+                width: 80
+            },
+            {
+                format: "jpeg",
+                height: 80,
+                path: "/posts_images/downloaded_image_a6d635da2a97f1b68b183289012bdcc0.jpg",
+                size: 1982,
+                url: "http://s3-us-west-2.amazonaws.com/s.cdpn.io/111863/profile/profile-80_2.jpg",
+                width: 80
+            }
+        ]
+    },
+
+    {
+        sourceService: "link",
+        sourceType: "link",
+        sourceUrl: "http://en.wikipedia.org/wiki/Main_Page",
+        title: "Wikipedia, the free encyclopedia",
+        description: "Main Page\n\nFrom Wikipedia, the free encyclopedia\n\n\nJump to:	navigation, search\n\n\n\n\nWelc",
+        images: [
+            {
+                format: "jpeg",
+                height: 81,
+                path: "/posts_images/downloaded_image_e5e2b2a25f69b005951d4f81e3fac613.jpg",
+                size: 3530,
+                url: "http://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Two_eastern_Lubber_grasshopers_%28Romalea_microptera%29%2C_mating.jpg/100pxTwo_eastern_Lubber_grasshopers_%28Romalea_microptera%29%2C_mating.jpg",
+                width: 100
+            },
+            {
+                format: "png",
+                height: 35,
+                path: "/posts_images/downloaded_image_e60b3455573c8c7eada10b28db1db610.png",
+                size: 2102,
+                url: "http://upload.wikimedia.org/wikipedia/en/7/7f/Wikibooks-logo-35px.png",
+                width: 35
+            },
+            {
+                format: "jpeg",
+                height: 120,
+                path: "/posts_images/downloaded_image_912310125c5f5a43ce4bc600d308b56b.jpg",
+                size: 3182,
+                url: "http://upload.wikimedia.org/wikipedia/en/thumb/a/ad/Carl_Nielsen_1917_crop.jpg/100px-Carl_Nielsen_1917_crop.jpg",
+                width: 100
+            },
+            {
+                format: "png",
+                height: 35,
+                path: "/posts_images/downloaded_image_ee464d8ad3a5d92c30945e7d0ffb00ba.png",
+                size: 2299,
+                url: "http://upload.wikimedia.org/wikipedia/en/b/bc/Meta-logo-35px.png",
+                width: 35
+            },
+            {
+                format: "png",
+                height: 41,
+                path: "/posts_images/downloaded_image_e0eb850cb2c3ea214fef92cee4be8abd.png",
+                size: 2053,
+                url: "http://upload.wikimedia.org/wikipedia/en/9/9d/Commons-logo-31px.png",
+                width: 31
+            },
+            {
+                format: "jpeg",
+                height: 120,
+                path: "/posts_images/downloaded_image_b4a5beae5efd918390478e8e37f8488b.jpg",
+                size: 4148,
+                url: "http://upload.wikimedia.org/wikipedia/en/thumb/6/69/American_Pharoah_cropped.jpg/72px-American_Pharoah_cropped.jpg",
+                width: 72
+            }
+        ]
+    },
+
+    {
+        sourceService: "link",
+        sourceType: "link",
+        sourceUrl: "https://dribbble.com/",
+        title: "Dribbble - Show and tell for designers",
+        description: "What are you working on?\nDribbble is show and tell for designers.\n Learn more\n Sign up\n \n \n \n \n \n \n \n \n PopularRecentMost ViewedMost Commented\n \n ",
+        images: [
+            {
+                format: "png",
+                height: 32,
+                path: "/posts_images/downloaded_image_efbc01f0bd8464b04d200b5204aabb40.png",
+                size: 1730,
+                url: "http://d13yacurqjgara.cloudfront.net/users/31752/avatars/mini/3207fbd57de3dafb59f2c3fc913460a6.png?1410553460",
+                width: 32,
+            },
+            {
+                format: "png",
+                height: 30,
+                path: "/posts_images/downloaded_image_2c06fcc69d92cee0e7371d20fbbcf62a.png",
+                size: 651,
+                url: "http://d13yacurqjgara.cloudfront.net/assets/icon-x-30-white-6252e41df2f2d686fd2939e92987428e.png",
+                width: 30
+            },
+            {
+                format: "png",
+                height: 22,
+                path: "/posts_images/downloaded_image_4232b5682d84b2db5c20ed40f05f810a.png",
+                size: 3067,
+                url: "http://d13yacurqjgara.cloudfront.net/assets/logo-bw-6420803e6a66d3fd129fd3c6a7456657.gif",
+                width: 86
+            },
+            {
+                format: "png",
+                height: 32,
+                path: "/posts_images/downloaded_image_64a211e445632b6ef486e0389460f43e.png",
+                size: 809,
+                url: "http://d13yacurqjgara.cloudfront.net/assets/icon-attach-16-2x-567ad2f047838a80e2d22809b530ce78.png",
+                width: 32
+            },
+            {
+                format: "png",
+                height: 32,
+                path: "/posts_images/downloaded_image_9d766bedd31f1a19ba836972ab1f4fd7.png",
+                size: 809,
+                url: "http://d13yacurqjgara.cloudfront.net/assets/icon-attach-16-2x-567ad2f047838a80e2d22809b530ce78.png",
+                width: 32
+            },
+            {
+                format: "png",
+                height: 32,
+                path: "/posts_images/downloaded_image_ffc0e235f578546a0f518ef0637a99a3.png",
+                size: 809,
+                url: "http://d13yacurqjgara.cloudfront.net/assets/icon-attach-16-2x-567ad2f047838a80e2d22809b530ce78.png",
+                width: 32
+            },
+            {
+                format: "png",
+                height: 32,
+                path: "/posts_images/downloaded_image_125f7d6b76569f84d186d3586c0673e6.png",
+                size: 1264,
+                url: "http://d13yacurqjgara.cloudfront.net/users/59947/avatars/mini/MUTI-01.jpg?1390990460",
+                width: 32
+            },
+            {
+                format: "png",
+                height: 32,
+                path: "/posts_images/downloaded_image_4d5c189807af923b55c7d57002d2581d.png",
+                size: 829,
+                url: "http://d13yacurqjgara.cloudfront.net/assets/icon-rebound-2x-29d53ec7d7c6507af9d07d342e1dfb16.png",
+                width: 32
+            },
+            {
+                format: "png",
+                height: 32,
+                path: "/posts_images/downloaded_image_a97b58560a75d33175612ec5332a7369.png",
+                size: 829,
+                url: "http://d13yacurqjgara.cloudfront.net/assets/icon-rebound-2x-29d53ec7d7c6507af9d07d342e1dfb16.png",
+                width: 32
+            },
+            {
+                format: "png",
+                height: 32,
+                path: "/posts_images/downloaded_image_0139fdaeade8a772a62af385299f2b43.png",
+                size: 829,
+                url: "http://d13yacurqjgara.cloudfront.net/assets/icon-rebound-2x-29d53ec7d7c6507af9d07d342e1dfb16.png",
+                width: 32
+            }
+        ]
+    }
+];
