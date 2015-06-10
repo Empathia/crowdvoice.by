728dd2fdbb5363140fbd430a7dd8fd96f067b72d
Noel Delgado <auhcsei@gmail.com>
2015-06-10 14:11:28 -0500

    add / PostCreator » fromSources markup and empty states

--- public/js/widgets/post-creators/post-creator-from-sources.js
+++ public/js/widgets/post-creators/post-creator-from-sources.js
@@ -1,8 +1,137 @@
-Class(CV, 'PostCreatorFromSources').inherits(Widget).includes(CV.WidgetUtils)({
+/* jshint multistr: true */
+var Checkit = require('checkit');
+
+Class(CV, 'PostCreatorFromSources').inherits(CV.PostCreator)({
+
+    ELEMENT_CLASS : 'cv-post-creator post-creator-from-sources',
+
+    HTML : '\
+    <div>\
+        <header class="cv-post-creator__header -clearfix">\
+            <div class="header-left -rel -float-left">\
+                <div class="from-sources-dropdown -full-height">\
+                    <svg class="-s18">\
+                        <use xlink:href="#svg-twitter-bird"></use>\
+                    </svg>\
+                    <svg class="arrow -s8 -color-grey">\
+                        <use xlink:href="#svg-arrow-down-1"></use>\
+                    </svg>\
+                </div>\
+            </div>\
+            <div class="header-right -rel -float-right">\
+                <button class="post-btn ui-btn -primary -pl2 -pr2">Post</button>\
+            </div>\
+            <div class="header-flex -rel">\
+            </div>\
+        </header>\
+        <div class="cv-post-creator__content -abs">\
+            <div class="from-sources-content-left -rel -color-bg-white -full-height -float-left">\
+                <div class="from-sources-no-results -color-grey-light -text-center">\
+                    <p>We found no results for ‘{{TERM}}’.<br/>Please refine your search and try again.</p>\
+                </div>\
+                <div class="cv-loader -abs">\
+                    <div class="ball-spin-fade-loader">\
+                        <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>\
+                    </div>\
+                </div>\
+            </div>\
+            <div class="from-sources-content-right -color-bg-grey-lighter -color-border-grey-light -full-height -float-left">\
+                <div class="from-sources-queue-onboarding -color-grey-light -text-center">\
+                    <p>Add here the posts you want to include in this voice.<br/>You’ll be able to edit their title and description.</p>\
+                </div>\
+            </div>\
+        </div>\
+        <div class="cv-post-creator__disable"></div>\
+    </div>\
+    ',
+
     prototype : {
+
+        el : null,
+        inputWrapper : null,
+        postButton : null,
+
+        _inputKeyPressHandlerRef : null,
+        _inputLastValue : null,
+        _checkitUrl : null,
+
         init : function init(config) {
-            Widget.prototype.init.call(this, config);
+            CV.PostCreator.prototype.init.call(this, config);
+
             console.log('new PostCreatorFromSources');
+
+            this.el = this.element[0];
+            this.inputWrapper = this.el.querySelector('.header-flex');
+            this.postButton = this.el.querySelector('.post-btn');
+
+            this.addCloseButton()._disablePostButton()._setup()._bindEvents();
+        },
+
+        /* Appends the InputClearable widget.
+         * @method _setup <private> [Function]
+         * @return [PostCreatorFromUrl]
+         */
+        _setup : function _setup() {
+            this.appendChild(
+                new CV.InputClearable({
+                    name : 'input',
+                    placeholder : 'Search for content',
+                    inputClass : '-block -lg -br0 -bw0 -full-height',
+                    className : '-full-height'
+                })
+            ).render(this.inputWrapper);
+
+            return this;
+        },
+
+        /* Binds the events for the InputClearable (keydown enter)
+         * @method _bindEvents <private> [Function]
+         * @return [PostCreatorFromUrl]
+         */
+        _bindEvents : function _bindEvents() {
+            CV.PostCreator.prototype._bindEvents.call(this);
+
+            if (typeof this.input === 'undefined') {
+                console.warn('PostCreatorFromUrl::Missing Input Widget');
+                return;
+            }
+
+            this._inputKeyPressHandlerRef = this._inputKeyPressHandler.bind(this);
+            this.input.getElement().addEventListener('keypress', this._inputKeyPressHandlerRef);
+
+            return this;
+        },
+
+        /* Handles the keypress event on the input. We are only interested on the ENTER key.
+         * @method _inputKeyPressHandler <private> [Function]
+         */
+        _inputKeyPressHandler : function _inputKeyPressHandler(ev) {
+            var inputValue;
+
+            if (ev.keyCode !== 13) {
+                return;
+            }
+
+            inputValue = this.input.getValue();
+
+            if (inputValue === this._inputLastValue) {
+                return;
+            }
+
+            this._inputLastValue = inputValue;
+
+            this._disablePostButton();
+        },
+
+        /* Disables the Post Button.
+         * @method _disablePostButton <private> [Function]
+         * @return [PostCreatorFromUrl]
+         */
+        _disablePostButton : function _disablePostButton() {
+            this.postButton.classList.add('-muted');
+            this.postButton.setAttribute('disabled', true);
+
+            return this;
         }
     }
 });
