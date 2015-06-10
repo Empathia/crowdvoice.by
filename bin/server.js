a7a0bdb7409f1e3f4fdaa43192c4d51c9b927eab
Pablo Gonzalez <pbglezc@gmail.com>
2015-06-10 16:04:09 -0500

    Add ImageUploader module. Include the module on Entity model.

--- bin/server.js
+++ bin/server.js
@@ -8,8 +8,11 @@ global.ForbiddenError = function ForbiddenError(message) {
 ForbiddenError.prototype = Object.create(Error.prototype);
 ForbiddenError.prototype.constructor = ForbiddenError;
 
-
 var application = require('neonode-core');
+
+// Load aws-sdk and S3
+var AWS = require('aws-sdk');
+global.amazonS3 = new AWS.S3(CONFIG.s3);
 
 global.ACL = require('./../lib/ACL/ACL.js');
 require('./../lib/ACL/visitor.js');
