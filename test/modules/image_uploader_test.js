3344be9e1f1110dbe99bf74af3cc904b13b62adb
Pablo Gonzalez <pbglezc@gmail.com>
2015-06-11 18:21:01 -0500

    Add background to voices.

--- test/modules/image_uploader_test.js
+++ test/modules/image_uploader_test.js
@@ -7,12 +7,12 @@ CONFIG.database.logQueries = false;
 console.log('');
 console.log('...');
 
-var m = new Post({id:3});
-var propertyName = 'image';
+var m = new Voice({id:3});
+var propertyName = 'background';
 
 m.uploadImage(propertyName, process.argv[2], function () {
+  console.log(m);
   Object.keys(m[propertyName].versions).forEach(function (version) {
     console.log(m[propertyName].url(version));
-    console.log(m);
   });
 });
