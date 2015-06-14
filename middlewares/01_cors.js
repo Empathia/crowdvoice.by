e8f13b2fa440f14acda815d8ada42b0735eb7ad2
Sergio de la Garza <sergio@delagarza.io>
2015-06-13 19:06:16 -0500

    Add response time

--- middlewares/01_cors.js
+++ middlewares/01_cors.js
@@ -2,7 +2,18 @@
 //                             CORS
 // *************************************************************************
 module.exports = function (req, res, next) {
+  var hrtime = function() {
+    var hrTime = process.hrtime();
+    return (hrTime[0] + (hrTime[1] / 1000000));
+  }
+
+  var startTime = hrtime();
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+
+  res.on('finish', function() {
+    logger.log('RESPONSE TIME: ' + ((hrtime() - startTime).toFixed(4) + 'ms' + "\n"));
+
+  })
   next();
 };
