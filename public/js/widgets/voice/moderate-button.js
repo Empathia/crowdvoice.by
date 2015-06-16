b9f6b5ab94e797ed8215f04e5bcbcfa120326a23
Noel Delgado <auhcsei@gmail.com>
2015-06-16 15:50:06 -0500

    add / voice » moderate button tooltip, tooltip » top-right

--- public/js/widgets/voice/moderate-button.js
+++ public/js/widgets/voice/moderate-button.js
@@ -2,10 +2,11 @@
 Class(CV, 'VoiceModerate').inherits(Widget)({
 
     HTML : '\
-    <button class="cv-button tiny primary -color-white">\
+    <button class="cv-button tiny primary -color-white ui-has-tooltip">\
         <svg class="voice-footer-svg">\
             <use xlink:href="#svg-moderate"></use>\
         </svg>\
+        <span class="ui-tooltip -top-right -nw">Moderate Content</span>\
     </button>',
 
     prototype : {
