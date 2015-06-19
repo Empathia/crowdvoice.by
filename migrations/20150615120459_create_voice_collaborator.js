63ee5b59bb938823bc6422eeb9f9ac443103354c
Sergio de la Garza <sergio@delagarza.io>
2015-06-19 16:17:29 -0500

    Include voice or organization objects in messages

--- migrations/20150615120459_create_voice_collaborator.js
+++ migrations/20150615120459_create_voice_collaborator.js
@@ -5,6 +5,7 @@ exports.up = function(knex, Promise) {
       t.increments('id').primary();
       t.integer('voice_id').defaultTo(0).index();
       t.integer('collaborator_id').defaultTo(0).index();
+      t.boolean('is_anonymous').defaultTo(false);
       t.index(['voice_id', 'collaborator_id'], 'voice_collaborators');
       t.timestamps();
     })
