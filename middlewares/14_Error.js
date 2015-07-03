2416b077d8e9477805d252d2016fe4333669c137
Noel Delgado <auhcsei@gmail.com>
2015-07-03 00:00:53 -0500

    add / 404, 403 new views

--- middlewares/14_Error.js
+++ middlewares/14_Error.js
@@ -5,15 +5,15 @@ module.exports = function(err, req, res, next) {
   res.format({
     html : function() {
       if (err.name && err.name === 'NotFoundError') {
-        return res.status(404).render('shared/404.html', {layout : false, message : err.message});
+        return res.status(404).render('shared/404.html', {layout : 'systemStatus', message : err.message});
       }
 
       if (err.name && err.name === 'ForbiddenError') {
-        return res.status(403).render('shared/403.html', {layout : false, message : err.message});
+        return res.status(403).render('shared/403.html', {layout : 'systemStatus', message : err.message});
       }
 
       res.status(500);
-      res.render('shared/500.html', {layout : false, error : err.stack})
+      res.render('shared/500.html', {layout : 'systemStatus', error : err.stack})
     },
     json : function() {
       if (err.name && err.name === 'NotFoundError') {
