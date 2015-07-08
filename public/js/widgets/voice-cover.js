a57347325d656f700e26d52f79460a997d5abf41
Noel Delgado <auhcsei@gmail.com>
2015-07-08 12:26:22 -0500

    fix / VoiceCover updated_at should be updatedAt

--- public/js/widgets/voice-cover.js
+++ public/js/widgets/voice-cover.js
@@ -9,7 +9,7 @@
  * title        {String} voice title (65 chars max)
  * description  {String} voice description
  * followers    {Array} voice entities followers
- * updated_at   {String} ISO date string
+ * updatedAt   {String} ISO date string
  */
 
 var moment = require('moment');
@@ -90,8 +90,8 @@ Class('VoiceCover').inherits(Widget).includes(CV.WidgetUtils)({
       this.dom.updateText(this.el.querySelector('.voice-cover-description'), this.description);
 
       this.dom.updateText(this.el.querySelector('.voice-cover-followers'), this.format.numberUS(this.followers.length));
-      this.dom.updateText(this.dateTimeElement, moment(this.updated_at).fromNow());
-      this.dom.updateAttr('datetime', this.dateTimeElement, this.updated_at);
+      this.dom.updateText(this.dateTimeElement, moment(this.updatedAt).fromNow());
+      this.dom.updateAttr('datetime', this.dateTimeElement, this.updatedAt);
 
       if (this.style == 'list'){
         this.element.addClass('list-style');
@@ -102,7 +102,7 @@ Class('VoiceCover').inherits(Widget).includes(CV.WidgetUtils)({
       }
 
       // 21 == 3 weeks (days)
-      if (moment().diff(moment(this.updated_at), 'days') <= 21) {
+      if (moment().diff(moment(this.updatedAt), 'days') <= 21) {
         this.addNewBadge();
       }
     },
