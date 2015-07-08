900f0b6fc8cebfdd1c2da77be1ebaccfb3c216b6
Noel Delgado <auhcsei@gmail.com>
2015-07-08 11:51:24 -0500

    add / more voice covers dummy data for the homepage

--- public/demo-data/voices.js
+++ public/demo-data/voices.js
@@ -101,6 +101,7 @@ module.exports = [
     type : '',
     hasActions : false
   },
+
   {
     url : '/jack-johnson/presidential-elections-2016',
     tags : [
@@ -161,6 +162,109 @@ module.exports = [
     description: 'The latest extension is the fourth since the recession began, which means some unemployed workers in the state could qualify for up to 99 weeks of benefits',
     followers : 172,
     updated_at: '2015-04-21T13:59:47Z',
+    gallery : [],
+    style : '',
+    type : '',
+    hasActions : false
+  },
+
+  {
+    url : '/jack-johnson/fukushima-disaster',
+    tags : [
+      {
+        name: 'Current Events',
+        url: '/topic/current-events'
+      },
+      {
+        name: 'Health',
+        url: '/topic/health'
+      }
+    ],
+    image_cover : '/img/sample/covers/feat-00.jpg',
+    images : {
+        small : { url : '/img/sample/covers/feat-00.jpg' },
+        card : { url : '/img/sample/covers/feat-00.jpg' }
+    },
+    owner : {
+      name : 'The Guardian',
+      profileName: 'guardian',
+      url: '/organization-test',
+      images : {
+          icon : {
+              url : '/img/sample/avatars/sm/org-00.jpg'
+          }
+      }
+    },
+    title: 'Continued Effects of the Fukushima Disaster',
+    description: 'On March 11, 2011, a tsunami and earthquake damaged the Fukushima Daiichi power plant in Fukushima, Japan. Subsequent equipment failures led to the release of nuclear material into the surrounding ground and ocean. It is regarded as the biggest nuclear disaster since Chernobyl. Initially, studies conducted by TEPCO, the company operating the plant, concluded that the risks',
+    followers : 3296,
+    updated_at: '2015-03-30T13:59:47Z',
+    gallery : [],
+    style : '',
+    type : '',
+    hasActions : false
+  },
+
+  {
+    url : '/jack-johnson/ferguson-unrest',
+    tags : [
+      {
+        name: 'Conflict',
+        url: '/topic/conflict'
+      }
+    ],
+    image_cover : '/img/sample/covers/feat-01.jpg',
+    images : {
+        small : { url : '/img/sample/covers/feat-00.jpg' },
+        card : { url : '/img/sample/covers/feat-01.jpg' }
+    },
+    owner : {
+      name : 'The Guardian',
+      profileName: 'guardian',
+      url: '/organization-test',
+      images : {
+          icon : {
+              url : '/img/sample/avatars/sm/org-00.jpg'
+          }
+      }
+    },
+    title: 'Ferguson Unrest',
+    description: 'An ongoing series of protests and civil disorder began the day after the fatal shooting of Michael Brown on August 9, 2014, in Ferguson, Missouri. The unrest sparked a vigorous debate',
+    followers : 288,
+    updated_at: '2015-04-23T13:59:47Z',
+    gallery : [],
+    style : '',
+    type : '',
+    hasActions : false
+  },
+
+  {
+    url : '/jack-johnson/civil-war-syria',
+    tags : [
+      {
+        name: 'Conflict',
+        url: '/topic/conflict'
+      }
+    ],
+    image_cover : '/img/sample/covers/feat-02.jpg',
+    images : {
+        small : { url : '/img/sample/covers/feat-00.jpg' },
+        card : { url : '/img/sample/covers/feat-02.jpg' }
+    },
+    owner : {
+      name : 'The Guardian',
+      profileName: 'guardian',
+      url: '/organization-test',
+      images : {
+          icon : {
+              url : '/img/sample/avatars/sm/org-00.jpg'
+          }
+      }
+    },
+    title: 'Civil War in Syria',
+    description: 'With thousands of schools destroyed in the conflict, and families being displaced from their homes and communities, maintaining any meaningful form of structured education',
+    followers : 763,
+    updated_at: '2015-04-22T13:59:47Z',
     gallery : [],
     style : '',
     type : '',
