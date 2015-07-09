aa5ff50e888dfe191aabd0e6b7eb8f996e2c5673
Noel Delgado <auhcsei@gmail.com>
2015-07-08 23:47:30 -0500

    update / homepage » display organization cards

--- public/js/views/home.js
+++ public/js/views/home.js
@@ -10,36 +10,35 @@ Class(CV, 'HomeView').includes(NodeSupport, CV.WidgetUtils)({
                 this[propertyName] = config[propertyName];
             }, this);
 
-            var featuredVoicesWrapper = document.querySelector('.homepage-featured-voices-container');
-            var featuredVoicesElements = [];
-            var categoriesHolder = document.querySelector('.homepage-category-list-row');
-            var orgsHolder = document.querySelector('.homepage-organization-cards-holder');
-            var orgsList = orgsHolder.querySelector('.slider-list');
-
+            /* STATS */
             [].slice.call(document.querySelectorAll('.stats .stats-number'), 0).forEach(function(number) {
                 this.dom.updateText(number, this.format.numberUS(number.textContent));
             }, this);
 
+            /* FEATURED VOICES */
+            var featuredVoicesWrapper = document.querySelector('.homepage-featured-voices-container');
+            var featuredVoicesElements = [];
             this.featuredVoicesData.forEach(function(voice, index) {
                 voice.name = 'featuredVoice_' + index;
-
-                this.appendChild(
-                    new VoiceCover(voice)
-                ).render(featuredVoicesWrapper);
-
+                this.appendChild( new VoiceCover(voice)).render(featuredVoicesWrapper);
                 featuredVoicesElements.push(this[voice.name].el);
             }, this);
 
-            var featuredVoices = new CV.ResponsiveWidth({
+            new CV.ResponsiveWidth({
                 container : featuredVoicesWrapper,
                 items : [].slice.call(featuredVoicesElements, 0),
                 minWidth : 300
             }).setup();
 
+            /* CATEGORIES */
+            var categoriesHolder = document.querySelector('.homepage-category-list-row');
             this.categoriesData.forEach(function(category) {
                 new CategoryCover(category).render(categoriesHolder);
             });
 
+            /* ORGANIZATIONS */
+            var orgsHolder = document.querySelector('.homepage-organization-cards-holder');
+            var orgsList = orgsHolder.querySelector('.slider-list');
             this.orgsData.map(function(org) {
                 var card = new CV.Card(org);
                 card.element[0].classList.add('slider-item');
