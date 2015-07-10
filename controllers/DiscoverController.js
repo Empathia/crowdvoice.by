af0cd389efd152c044b5327c0588ebc18f4a710a
greduan <me@greduan.com>
2015-07-10 17:42:01 -0500

    some work on DiscoverController
    but pushing here because Checo wants to see the code as it is, since
    /discover/new/voices gives a 404 for some reason.

--- controllers/DiscoverController.js
+++ controllers/DiscoverController.js
@@ -1,9 +1,29 @@
-var DiscoverController = Class('DiscoverController').includes('BlackListFilter')({
+var BlackListFilter = require(__dirname + '/BlackListFilter.js')
+
+var dbLimit = 50
+
+var DiscoverController = Class('DiscoverController').includes(BlackListFilter)({
   prototype: {
-    newVoices: function (req, res, next) {},
+    newVoices: function (req, res, next) {
+      Voice.find(['SELECT * FROM "Voices" ORDER BY created_at DESC LIMIT ?', dbLimit], function (err, result) {
+        if (err) { return next(err) }
 
-    newPeople: function (req, res, next) {},
+        res.json(result)
+      })
+    },
 
-    newOrganizations: function (req, res, next) {},
+    newPeople: function (req, res, next) {
+      Entity.find({
+        type: 'person'
+      })
+    },
+
+    newOrganizations: function (req, res, next) {
+      Entity.find({
+        type: 'organization'
+      })
+    },
   },
 })
+
+module.exports = new DiscoverController()
