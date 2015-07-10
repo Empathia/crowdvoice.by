bb35f6013dba2200624ede8444c13f8bcf9d172c
Sergio de la Garza <sergio@delagarza.io>
2015-07-10 18:00:41 -0500

    Fix discovery newVoices

--- controllers/DiscoverController.js
+++ controllers/DiscoverController.js
@@ -1,24 +1,22 @@
-var BlackListFilter = require(__dirname + '/BlackListFilter.js')
-
 var dbLimit = 50
 
-var DiscoverController = Class('DiscoverController').includes(BlackListFilter)({
+var DiscoverController = Class('DiscoverController')({
   prototype: {
-    newVoices: function (req, res, next) {
-      Voice.find(['SELECT * FROM "Voices" ORDER BY created_at DESC LIMIT ?', dbLimit], function (err, result) {
+    newVoices: function(req, res, next) {
+      Voice.find(['status = ? ORDER BY created_at DESC LIMIT ?', [Voice.STATUS_PUBLISHED, dbLimit]], function (err, result) {
         if (err) { return next(err) }
 
         res.json(result)
       })
     },
 
-    newPeople: function (req, res, next) {
+    newPeople: function(req, res, next) {
       Entity.find({
         type: 'person'
       })
     },
 
-    newOrganizations: function (req, res, next) {
+    newOrganizations: function(req, res, next) {
       Entity.find({
         type: 'organization'
       })
