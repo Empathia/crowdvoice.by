667718d3174d15d74dbe73a4e631ffe6c7fcec00
Noel Delgado <auhcsei@gmail.com>
2015-07-12 03:17:03 -0500

    add / VoiceCover » anchor to voice title

--- public/js/widgets/voice-cover.js
+++ public/js/widgets/voice-cover.js
@@ -1,4 +1,3 @@
-/* jshint multistr: true */
 /**
  * VoiceCover Widget
  *
@@ -29,7 +28,7 @@ Class('VoiceCover').inherits(Widget).includes(CV.WidgetUtils)({
         <div class="voice-cover-main-image-wrapper">\
           <div class="voice-cover-main-image -img-cover"></div>\
         </div>\
-        <a class="voice-cover-hover-overlay" href="{{voice-url}}">\
+        <a class="voice-cover-hover-overlay" data-voice-anchor href="{{voice-url}}">\
           <button class="voice-cover-hover-overlay-button cv-button tiny -font-semi-bold">View Voice</button>\
         </a>\
       </div>\
@@ -40,7 +39,9 @@ Class('VoiceCover').inherits(Widget).includes(CV.WidgetUtils)({
             <span class="author-username">{{voice-owner-name}}</span>\
           </a>\
         </div>\
-        <h2 class="voice-cover-title -font-bold">{{voice-title}}</h2>\
+        <h2 class="voice-cover-title -font-bold">\
+          <a class="voice-cover-title-anchor" data-voice-anchor href="{{voice-url}}">{{voice-title}}</a>\
+        </h2>\
         <p class="voice-cover-description">{{voice-description}}</p>\
         <div class="meta">\
           <span class="voice-cover-followers">{{voice-followers}}</span> followers &middot;&nbsp;\
@@ -70,12 +71,29 @@ Class('VoiceCover').inherits(Widget).includes(CV.WidgetUtils)({
       this.tagListElement = this.element.find('.cv-tags');
       this.voiceCoverElement = this.element.find('.voice-cover');
       this.dateTimeElement = this.el.querySelector('.voice-cover-datetime');
+      this.voiceAnchors = [].slice.call(this.el.querySelectorAll('[data-voice-anchor]'), 0);
 
-      this.dom.updateAttr('href', this.el.querySelector('.voice-cover-hover-overlay'), this.owner.profileName + '/' + this.slug + '/');
-      this.dom.updateAttr('title', this.el.querySelector('.voice-cover-hover-overlay'), this.title + ' voice');
+     this._updateValues();
+
+      // is new? no older than 21 days == 3 weeks
+      if (moment().diff(moment(this.updatedAt), 'days') <= 21) {
+        this.addNewBadge();
+      }
+    },
+
+    /* Update the widget's elements values with the received config
+     * @method _updateValues <private> [Function]
+     */
+    _updateValues : function _updateValues() {
+      this.voiceAnchors.forEach(function(anchor) {
+        this.dom.updateAttr('href', anchor, this.owner.profileName + '/' + this.slug + '/');
+        this.dom.updateAttr('title', anchor, this.title + ' voice');
+      }, this);
+
       this.dom.updateBgImage(this.el.querySelector('.voice-cover-main-image'), this.images.card.url);
+
       if (this.topics.length) {
-          this.createTopics(this.topics);
+        this.createTopics(this.topics);
       }
 
       var authorFullname = this.owner.lastname ? (this.owner.name + ' ' + this.owner.lastname) : this.owner.name;
@@ -84,24 +102,19 @@ Class('VoiceCover').inherits(Widget).includes(CV.WidgetUtils)({
       this.dom.updateAttr('src', this.el.querySelector('.author-avatar'), this.owner.images.icon.url);
       this.dom.updateText(this.el.querySelector('.author-username'), 'by ' + authorFullname);
 
-      this.dom.updateText(this.el.querySelector('.voice-cover-title'), this.title);
+      this.dom.updateText(this.el.querySelector('.voice-cover-title-anchor'), this.title);
       this.dom.updateText(this.el.querySelector('.voice-cover-description'), this.description);
 
       this.dom.updateText(this.el.querySelector('.voice-cover-followers'), this.format.numberUS(this.followers.length));
       this.dom.updateText(this.dateTimeElement, moment(this.updatedAt).fromNow());
       this.dom.updateAttr('datetime', this.dateTimeElement, this.updatedAt);
-
-      // is new? no older than 21 days == 3 weeks
-      if (moment().diff(moment(this.updatedAt), 'days') <= 21) {
-        this.addNewBadge();
-      }
     },
 
     /**
      * Creates a tag per topic that is tagged to the topic and appends them.
      * @method createTopics <private> [Function]
      * @params tags <required> [Array] list of topics tagged to the voice
-     * @return undefined
+     * @return CV.VoiceCover
      */
     createTopics : function createTopics(topics) {
       topics.forEach(function(topic) {
@@ -123,7 +136,7 @@ Class('VoiceCover').inherits(Widget).includes(CV.WidgetUtils)({
     /**
      * Appends the new badge to the voiceCoverElement.
      * @method addNewBadge <private> [Function]
-     * @return undefined
+     * @return CV.VoiceCover
      */
     addNewBadge : function addNewBadge() {
       var badge = $(this.constructor.IS_NEW_BADGE_HTML);
