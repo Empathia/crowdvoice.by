4830ca08cde2ac6d8f5d74f52ce16854da56692f
greduan <me@greduan.com>
2015-07-13 17:44:14 -0500

    /browse/featured basic routes, views and controller

---
+++ controllers/BrowseController.js
@@ -0,0 +1,17 @@
+var BrowseController = Class('BrowseController')({
+  prototype: {
+    featuredVoices: function (req, res, next) {
+      //
+    },
+
+    featuredPeople: function (req, res, next) {
+      //
+    },
+
+    featuredOrganizations: function (req, res, next) {
+      //
+    },
+  },
+})
+
+module.exports = new BrowseController()
