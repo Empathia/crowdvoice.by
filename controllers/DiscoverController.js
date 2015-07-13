ebee0d8e7867dcf7ec6edfd6a36636d3a1a469cc
greduan <me@greduan.com>
2015-07-13 09:51:16 -0500

    /discover/new/{voices,people,organizations} works
    but it doesn't yet return HTML, only JSON

--- controllers/DiscoverController.js
+++ controllers/DiscoverController.js
@@ -1,4 +1,4 @@
-var dbLimit = 50
+var dbLimit = 50 // how many posts we can ask for at a time
 
 var DiscoverController = Class('DiscoverController')({
   prototype: {
@@ -6,19 +6,26 @@ var DiscoverController = Class('DiscoverController')({
       Voice.find(['status = ? ORDER BY created_at DESC LIMIT ?', [Voice.STATUS_PUBLISHED, dbLimit]], function (err, result) {
         if (err) { return next(err) }
 
+        // TODO: answer HTML
         res.json(result)
       })
     },
 
     newPeople: function(req, res, next) {
-      Entity.find({
-        type: 'person'
+      Entity.find(['type = ? AND NOT is_anonymous = ? ORDER BY created_at DESC LIMIT ?', ['person', 't', dbLimit]], function (err, result) {
+        if (err) { return next(err) }
+
+        // TODO: answer HTML
+        res.json(result)
       })
     },
 
     newOrganizations: function(req, res, next) {
-      Entity.find({
-        type: 'organization'
+      Entity.find(['type = ? ORDER BY created_at DESC LIMIT ?', ['organization', dbLimit]], function (err, result) {
+        if (err) { return next(err) }
+
+        // TODO: answer HTML
+        res.json(result)
       })
     },
   },
