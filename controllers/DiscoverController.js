a7e2a1d2dd71a089ca6b3855c2322cabc742e4eb
Rasta <rastafario@gmail.com>
2015-07-14 18:03:49 -0500

    Added initial discover screens

--- controllers/DiscoverController.js
+++ controllers/DiscoverController.js
@@ -36,6 +36,10 @@ var findTrending = function (resultFromKnex, name) {
 
 var DiscoverController = Class('DiscoverController')({
   prototype: {
+    newIndex : function(req, res, next){
+      res.render('discover/new');
+    },
+
     newVoices: function(req, res, next) {
       Voice.find(['status = ? ORDER BY created_at DESC LIMIT ?', [Voice.STATUS_PUBLISHED, dbLimit]], function (err, result) {
         if (err) { return next(err) }
@@ -97,6 +101,10 @@ var DiscoverController = Class('DiscoverController')({
           })
         })
       })
+    },
+
+    trendingIndex : function(req, res, next){
+      res.render('discover/trending');
     },
 
     trendingVoices: function (req, res, next) {
@@ -203,6 +211,9 @@ var DiscoverController = Class('DiscoverController')({
         })
       })
     },
+    recommendedIndex : function(req, res, next){
+      res.render('discover/recommended');
+    }
   },
 })
 
