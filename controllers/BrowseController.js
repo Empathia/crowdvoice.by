43fb928e82c6b32e6a330de1df7c34d131bd81a9
greduan <me@greduan.com>
2015-07-14 10:36:59 -0500

    BrowseController.featuredPeople works now
    had to rename some DB stuff cause I messed up, also added a .build to a
    EntitiesPresenter in DiscoverController

--- controllers/BrowseController.js
+++ controllers/BrowseController.js
@@ -22,11 +22,51 @@ var BrowseController = Class('BrowseController')({
     },
 
     featuredPeople: function (req, res, next) {
-      //
+      async.waterfall([
+        function (callback) {
+          FeaturedPerson.all(callback)
+        },
+
+        function (allFeaturedPeople, callback) {
+          var featuredPeopleIds = allFeaturedPeople.map(function (val) {
+            return val.entityId
+          })
+          Entity.whereIn('id', featuredPeopleIds, callback)
+        },
+
+        function (featuredPeopleEntities, callback) {
+          EntitiesPresenter.build(featuredPeopleEntities, req.currentPerson, callback)
+        },
+      ], function (err, result) {
+        if (err) { return next(err) }
+
+        res.format({
+          json: function () {
+            res.json(result)
+          },
+        })
+      })
     },
 
     featuredOrganizations: function (req, res, next) {
-      //
+      FeaturedOrganization.all(function (err, allFeaturedOrgs) {
+        if (err) { return next(err) }
+
+        EntitiesPresenter.build(allFeaturedOrgs, req.currentPerson, function (err, presenterOrgs) {
+          if (err) { return next(err) }
+
+          res.format({
+            html: function () {
+              res.locals.featuredOrganizations = presenterOrgs
+              req.featuredOrganizations = presenterOrgs
+              res.render('browse/featured/organizations')
+            },
+            json: function () {
+              res.json(presenterOrgs)
+            },
+          })
+        })
+      })
     },
   },
 })
