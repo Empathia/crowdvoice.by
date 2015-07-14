1e9125dff0b4703302e74471e28aadeb0a79b657
greduan <me@greduan.com>
2015-07-14 10:04:17 -0500

    BrowseController.featuredVoices has been added

--- controllers/BrowseController.js
+++ controllers/BrowseController.js
@@ -1,7 +1,24 @@
 var BrowseController = Class('BrowseController')({
   prototype: {
     featuredVoices: function (req, res, next) {
-      //
+      FeaturedVoice.all(function (err, allFeaturedVoices) {
+        if (err) { return next(err) }
+
+        EntitiesPresenter.build(allFeaturedVoices, req.currentPerson, function (err, presenterVoices) {
+          if (err) { return next(err) }
+
+          res.format({
+            html: function () {
+              res.locals.featuredVoices = presenterVoices
+              req.featuredVoices = presenterVoices
+              res.render('browse/featured/voices')
+            },
+            json: function () {
+              res.json(presenterVoices)
+            },
+          })
+        })
+      })
     },
 
     featuredPeople: function (req, res, next) {
