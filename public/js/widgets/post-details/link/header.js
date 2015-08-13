3372c1cb808458393e648f1ed326f6628bac22e6
Noel Delgado <auhcsei@gmail.com>
2015-08-13 02:56:01 -0500

    add / PostDetail, link, view original button

--- public/js/widgets/post-details/link/header.js
+++ public/js/widgets/post-details/link/header.js
@@ -4,20 +4,20 @@ Class(CV, 'PostDetailLinkHeader').inherits(Widget).includes(CV.WidgetUtils)({
     ELEMENT_CLASS : 'cv-post-detail-link__header',
     HTML : '\
         <div>\
-            <div class="-float-left">\
-                <p data-title>Suspend Dictum Feugiat Nisl Ut</p>\
-                <div>\
+            <div class="cv-post-detail-link__header-actions -float-right">\
+                <a class="header-actions-view-original-btn cv-button tiny -m0" target="_blank">View Original</a>\
+            </div>\
+            <div class="-overflow-hidden">\
+                <p data-title class="cv-post-detail-link__header-title -font-bold -ellipsis"></p>\
+                <div class="cv-post-detail-link__header-meta">\
                     <span>on <time data-date datetime></time></span>\
-                    <div class="-inline-block">\
-                        <svg class="-s16">\
+                    <div class="-inline-block -ml1">\
+                        <svg class="-s14">\
                             <use xlink:href="#svg-save-outline"></use>\
                             <span data-saved></span>\
                         </svg>\
                     </div>\
                 </div>\
-            </div>\
-            <div class="-float-right">\
-                <button class="cv-button tiny">View Original</button>\
             </div>\
         </div>',
 
@@ -28,6 +28,7 @@ Class(CV, 'PostDetailLinkHeader').inherits(Widget).includes(CV.WidgetUtils)({
             this.titleElement = this.el.querySelector('[data-title]');
             this.dateElement = this.el.querySelector('[data-date]');
             this.savedElement = this.el.querySelector('[data-saved]');
+            this.viewOriginalBtn = this.el.querySelector('.header-actions-view-original-btn');
         },
 
         update : function update(data) {
@@ -35,6 +36,7 @@ Class(CV, 'PostDetailLinkHeader').inherits(Widget).includes(CV.WidgetUtils)({
             this.dom.updateText(this.dateElement, moment(data.publishedAt).format('MMM DD, YYYY'));
             this.dom.updateAttr('datetime', this.dateElement, data.publishedAt);
             this.dom.updateText(this.savedElement, data.totalSaves || 0);
+            this.dom.updateAttr('href', this.viewOriginalBtn, data.sourceUrl);
         }
     }
 });
