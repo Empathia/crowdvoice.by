dc92e12f21b6eae9336ecb10b84d626bf521e170
Noel Delgado <auhcsei@gmail.com>
2015-08-13 16:15:44 -0500

    update / post creators error,success icon colors

--- public/js/widgets/post-creators/error.js
+++ public/js/widgets/post-creators/error.js
@@ -1,7 +1,7 @@
 Class(CV, 'PostCreatorErrorTemplate').inherits(Widget)({
     HTML : '\
         <div class="cv-post-creator__error-template -rel -br1">\
-            <svg class="center-icon -abs -color-grey-light">\
+            <svg class="center-icon -abs -color-negative">\
                 <use xlink:href="#svg-circle-x"></use>\
             </svg>\
         </div>'
--- public/js/widgets/post-creators/success.js
+++ public/js/widgets/post-creators/success.js
@@ -1,7 +1,7 @@
 Class(CV, 'PostCreatorSuccessTemplate').inherits(Widget)({
     HTML : '\
         <div class="cv-post-creator__success-template -rel -br1">\
-            <svg class="center-icon -abs -color-grey-light">\
+            <svg class="center-icon -abs -color-positive">\
                 <use xlink:href="#svg-thumbs-up"></use>\
             </svg>\
         </div>'
