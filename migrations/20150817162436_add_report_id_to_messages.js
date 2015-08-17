6ba12582d145f3d34931adf4e8231004ca4e70d6
greduan <me@greduan.com>
2015-08-17 17:40:39 -0500

    add an organization prop to messages type 'report'

--- migrations/20150817162436_add_report_id_to_messages.js
+++ migrations/20150817162436_add_report_id_to_messages.js
@@ -1,16 +1,16 @@
 
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table('Reports', function (t) {
-      t.integer('message_id').index();
+    knex.schema.table('Messages', function (t) {
+      t.integer('report_id').index();
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.table('Reports', function (t) {
-      t.dropColumn('message_id');
+    knex.schema.table('Messages', function (t) {
+      t.dropColumn('report_id');
     })
   ]);
   knex.schema
