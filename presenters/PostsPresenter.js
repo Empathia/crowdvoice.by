0b9ae0f5be3194ebe3415cc924bb7f8c6d65b207
Sergio de la Garza <sergio@delagarza.io>
2015-08-19 11:25:56 -0500

    Add voice to post presenter

--- presenters/PostsPresenter.js
+++ presenters/PostsPresenter.js
@@ -24,8 +24,9 @@ var PostsPresenter = Module('PostsPresenter')({
       post.postImages = images;
 
       async.series([
+
         // votes
-        function (done) {
+        function(done) {
           if (currentPerson) {
             Vote.find({
               'entity_id' : hashids.decode(currentPerson.id)[0],
@@ -48,7 +49,7 @@ var PostsPresenter = Module('PostsPresenter')({
         },
 
         // saved
-        function (done) {
+        function(done) {
           if (currentPerson) {
             SavedPost.find({
               entity_id: hashids.decode(currentPerson.id)[0],
@@ -81,14 +82,31 @@ var PostsPresenter = Module('PostsPresenter')({
 
             return done();
           });
-        }
-      ], function (err) {
+        }, function(done) {
+
+          // voice
+          Voice.find({ 'voice_id' : postInstance.voiceId }, function(err, result) {
+            if (err) {
+              return done(err);
+            }
+
+            VoicesPresenter.build(result, currentPerson, function(err, voices) {
+              if (err) {
+                return done(err);
+              }
+
+              post.voice = voices[0];
+
+              done();
+            });
+          });
+        } ], function(err) {
         if (err) { return next(err); }
 
         response.push(post);
         return next();
       });
-    }, function (err) {
+    }, function(err) {
       callback(err, response);
     });
   }
