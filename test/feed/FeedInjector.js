f41b403485353d07834f64a9ebfb8cce0a376097
greduan <me@greduan.com>
2015-08-27 16:19:24 -0500

    entityFollowsVoice tests pass

--- test/feed/FeedInjector.js
+++ test/feed/FeedInjector.js
@@ -7,8 +7,6 @@ var FeedInjector = require(__dirname + '/../../lib/FeedInjector.js')
 //test('', function (t) {
 //})
 
-require(__dirname + '/reset.js')
-
 test('entityFollowsEntity', function (t) {
   t.plan(4)
 
@@ -25,7 +23,7 @@ test('entityFollowsEntity', function (t) {
       }
 
       t.deepEqual(feedAction[0], {
-        id: 1,
+        id: feedAction[0].id,
         itemType: 'entity',
         itemId: 4,
         action: 'followed',
@@ -43,7 +41,48 @@ test('entityFollowsEntity', function (t) {
         t.deepEqual(notifs.length, 2, 'There is the correct amount of notifications')
 
         t.deepEqual(notifs[0].followerId, 2, 'Notification entry #1 is correct')
-        t.equal(notifs[1].followerId, 3, 'Notification entry #2 is correct') })
+        t.deepEqual(notifs[1].followerId, 3, 'Notification entry #2 is correct')
+      })
+    })
+  })
+})
+
+test('entityFollowsVoice', function (t) {
+  t.plan(4)
+
+  FeedInjector().inject(1, 'who entityFollowsVoice', { voiceId: 1 }, function (err) {
+    if (err) {
+      t.fail()
+      return console.log(err)
+    }
+
+    FeedAction.findById(2, function (err, feedAction) {
+      if (err) {
+        t.fail()
+        return console.log(err)
+      }
+
+      t.deepEqual(feedAction[0], {
+        id: feedAction[0].id,
+        itemType: 'voice',
+        itemId: 1,
+        action: 'followed',
+        who: 1,
+        createdAt: feedAction[0].createdAt,
+        updatedAt: feedAction[0].updatedAt,
+      }, 'The FeedAction entry is correct')
+
+      Notification.find(['action_id = ? ORDER BY follower_id ASC', [feedAction[0].id]], function (err, notifs) {
+        if (err) {
+          t.fail()
+          return console.log(err)
+        }
+
+        t.deepEqual(notifs.length, 2, 'There is the correct amount of notifications')
+
+        t.deepEqual(notifs[0].followerId, 2, 'Notification entry #1 is correct')
+        t.deepEqual(notifs[1].followerId, 3, 'Notification entry #2 is correct')
+      })
     })
   })
 })
