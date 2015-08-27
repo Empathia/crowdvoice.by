a5b1738948377fee93af2d62ac914f1edf632fd3
greduan <me@greduan.com>
2015-08-27 16:46:10 -0500

    entityBecomesOrgPublicMember

--- test/feed/FeedInjector.js
+++ test/feed/FeedInjector.js
@@ -204,3 +204,44 @@ test('entityUpdatesBackground', function (t) {
     })
   })
 })
+
+test('entityBecomesOrgPublicMember', function (t) {
+  t.plan(4)
+
+  FeedInjector().inject(1, 'who entityBecomesOrgPublicMember', { entityId: 4 }, function (err) {
+    if (err) {
+      t.fail()
+      return console.log(err)
+    }
+
+    FeedAction.findById(6, function (err, feedAction) {
+      if (err) {
+        t.fail()
+        return console.log(err)
+      }
+
+      t.deepEqual(feedAction[0], {
+        id: feedAction[0].id,
+        itemType: 'entity',
+        itemId: 4,
+        action: 'became public member',
+        who: 1,
+        createdAt: feedAction[0].createdAt,
+        updatedAt: feedAction[0].updatedAt,
+      }, 'The FeedAction entry is correct')
+
+      Notification.find(['action_id = ? ORDER BY follower_id ASC', [feedAction[0].id]], function (err, notifs) {
+        if (err) {
+          t.fail()
+          return console.log(err)
+        }
+
+        t.deepEqual(notifs.length, 2, 'There is the correct amount of notifications')
+
+        t.deepEqual(notifs[0].followerId, 2, 'Notification entry #1 is correct')
+        t.deepEqual(notifs[1].followerId, 3, 'Notification entry #2 is correct')
+      })
+    })
+  })
+})
+
