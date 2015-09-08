b5a0cddf8ad1a4c7299146733d6009cb8ccf1fe1
greduan <me@greduan.com>
2015-09-08 17:35:16 -0500

    redesign Admin.FeaturedPeopleController routes

--- controllers/admin/FeaturedPeopleController.js
+++ controllers/admin/FeaturedPeopleController.js
@@ -2,7 +2,28 @@ Admin.FeaturedPeopleController = Class(Admin, 'FeaturedPeopleController')({
 
   prototype: {
 
-    // get all featured people
+    // GET
+    // get entity and populate req.featuredPerson and res.locals.featuredPerson
+    getEntity: function (req, res, next) {
+      Entity.findById(hashids.decode(req.params.entityId)[0], function (err, result) {
+        if (err) { return next(err) }
+
+        req.featuredPerson = result[0]
+
+        EntitiesPresenter.build(result, req.currentPerson, function (err, entities) {
+          if (err) { return next(err) }
+
+          res.locals.featuredPerson = entities[0]
+
+          res.render('admin/featuredPeople/show.html', { layout: 'admin' })
+
+          return next()
+        })
+      })
+    },
+
+    // GET
+    // get all featured people and render index view
     index: function (req, res, next) {
       ACL.isAllowed('index', 'admin.featuredPeople', req.role, {}, function (err, isAllowed) {
         if (err) { return next(err) }
@@ -25,7 +46,8 @@ Admin.FeaturedPeopleController = Class(Admin, 'FeaturedPeopleController')({
       })
     },
 
-    // get featured person by :entityId
+    // GET
+    // render view for viewing a featured person
     show: function (req, res, next) {
       ACL.isAllowed('show', 'admin.featuredPeople', req.role, {}, function (err, isAllowed) {
         if (err) { return next(err) }
@@ -34,20 +56,12 @@ Admin.FeaturedPeopleController = Class(Admin, 'FeaturedPeopleController')({
           return next(new ForbiddenError('not an admin'))
         }
 
-        Entity.findById(hashids.decode(req.params.entityId)[0], function (err, result) {
-          if (err) { return next(err) }
-
-          EntitiesPresenter.build(result, req.currentPerson, function (err, entities) {
-            if (err) { return next(err) }
-
-            res.locals.person = entities[0]
-
-            res.render('admin/featuredPeople/show.html', { layout: 'admin' })
-          })
-        })
+        res.render('admin/featuredPeople/show.html', { layout: 'admin' })
       })
     },
 
+    // GET
+    // render view for new entity
     new: function (req, res, next) {
       ACL.isAllowed('new', 'admin.featuredPeople', req.role, {}, function (err, isAllowed) {
         if (err) { return next(err) }
@@ -58,6 +72,8 @@ Admin.FeaturedPeopleController = Class(Admin, 'FeaturedPeopleController')({
       })
     },
 
+    // POST
+    // create new entity from input
     create: function (req, res, next) {
       ACL.isAllowed('create', 'admin.featuredPeople', req.role, {}, function (err, isAllowed) {
         if (err) { return next(err) }
@@ -68,6 +84,8 @@ Admin.FeaturedPeopleController = Class(Admin, 'FeaturedPeopleController')({
       })
     },
 
+    // GET
+    // render view for editing entity
     edit: function (req, res, next) {
       ACL.isAllowed('edit', 'admin.featuredPeople', req.role, {}, function (err, isAllowed) {
         if (err) { return next(err) }
@@ -78,6 +96,8 @@ Admin.FeaturedPeopleController = Class(Admin, 'FeaturedPeopleController')({
       })
     },
 
+    // POST
+    // update entity from input
     update: function (req, res, next) {
       ACL.isAllowed('update', 'admin.featuredPeople', req.role, {}, function (err, isAllowed) {
         if (err) { return next(err) }
@@ -88,6 +108,8 @@ Admin.FeaturedPeopleController = Class(Admin, 'FeaturedPeopleController')({
       })
     },
 
+    // DELETE
+    // delete req.featuredPerson entity
     destroy: function (req, res, next) {
       ACL.isAllowed('destroy', 'admin.featuredPeople', req.role, {}, function (err, isAllowed) {
         if (err) { return next(err) }
