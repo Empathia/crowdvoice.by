6b5f61e882fa35d0b111dcfa312c52603312ed8b
greduan <me@greduan.com>
2015-09-08 16:12:09 -0500

    Add Featured{People,Organizations,Voices} admin controllers

---
+++ controllers/admin/FeaturedPeopleController.js
@@ -0,0 +1,30 @@
+Admin.FeaturedPeopleController = Class(Admin, 'FeaturedPeopleController')({
+
+  prototype: {
+
+    index: function (req, res, next) {
+    },
+
+    show: function (req, res, next) {
+    },
+
+    new: function (req, res, next) {
+    },
+
+    create: function (req, res, next) {
+    },
+
+    edit: function (req, res, next) {
+    },
+
+    update: function (req, res, next) {
+    },
+
+    destroy: function (req, res, next) {
+    },
+
+  },
+
+})
+
+module.exports = new Admin.FeaturedPeopleController()
