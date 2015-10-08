15c16fabc4edb59e44567b18adacfb19c8989871
Rasta <rastafario@gmail.com>
2015-10-08 17:18:42 -0500

    Added new featured dummy action

--- controllers/admin/FeaturedEntitiesController.js
+++ controllers/admin/FeaturedEntitiesController.js
@@ -223,7 +223,7 @@ Admin.FeaturedEntitiesController = Class(Admin, 'FeaturedEntitiesController')({
               featuredEntity
 
             async.each(featuredEntities, function (val, next) {
-              featuredEntity = new global['Featured' + inflection.tramsform(req.params.entityType, ['capitalize', 'singularize'])](val)
+              featuredEntity = new global['Featured' + inflection.transform(req.params.entityType, ['capitalize', 'singularize'])](val)
               featuredEntity.position = realIds.indexOf(val)
 
               featuredEntity.save(next)
