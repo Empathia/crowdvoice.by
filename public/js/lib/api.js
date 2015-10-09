33d0f09711e6c7086d564269ab16481abe7b77dd
Noel Delgado <auhcsei@gmail.com>
2015-10-09 02:52:58 -0500

    add / 4 new API endpoints for entities - getEntityVoices - getEntityFollowers - getEntityFollowedVoices - getEntityFollowedEntities

--- public/js/lib/api.js
+++ public/js/lib/api.js
@@ -9,6 +9,94 @@ module.exports = {
     /**************************************************************************
      * VOICES
      *************************************************************************/
+    /* Gets :profileName's Published Voices
+     * @argument args.profileName <required> [String] the voice owner profileName
+     * @argument callback <required> [Function]
+     */
+    getEntityVoices : function getEntityVoices(args, callback) {
+        if (!args.profileName || !callback) {
+            throw new Error('Missing required params');
+        }
+
+        if ((typeof callback).toLowerCase() !== "function") {
+            throw new Error('Callback should be a function');
+        }
+
+        $.ajax({
+            type : "GET",
+            url :'/' + args.profileName + '/voices',
+            headers : {'csrf-token' : this.token},
+            success : function success(data) { callback(false, data); },
+            error : function error(err) { callback(true, err); }
+        });
+    },
+
+    /* Gets :profileName's followers
+     * @argument args.profileName <required> [String] the voice owner profileName
+     * @argument callback <required> [Function]
+     */
+    getEntityFollowers : function getEntityFollowers(args, callback) {
+        if (!args.profileName || !callback) {
+            throw new Error('Missing required params');
+        }
+
+        if ((typeof callback).toLowerCase() !== "function") {
+            throw new Error('Callback should be a function');
+        }
+
+        $.ajax({
+            type : "GET",
+            url :'/' + args.profileName + '/followers',
+            headers : {'csrf-token' : this.token},
+            success : function success(data) { callback(false, data); },
+            error : function error(err) { callback(true, err); }
+        });
+    },
+
+    /* Get published voices followed by :profileName
+     * @argument args.profileName <required> [String] the voice owner profileName
+     * @argument callback <required> [Function]
+     */
+    getEntityFollowedVoices : function getEntityFollowedVoices(args, callback) {
+        if (!args.profileName || !callback) {
+            throw new Error('Missing required params');
+        }
+
+        if ((typeof callback).toLowerCase() !== "function") {
+            throw new Error('Callback should be a function');
+        }
+
+        $.ajax({
+            type : "GET",
+            url :'/' + args.profileName + '/voicesFollowed',
+            headers : {'csrf-token' : this.token},
+            success : function success(data) { callback(false, data); },
+            error : function error(err) { callback(true, err); }
+        });
+    },
+
+    /* Get people and organizations followed by :profileName
+     * @argument args.profileName <required> [String] the voice owner profileName
+     * @argument callback <required> [Function]
+     */
+    getEntityFollowedEntities : function getEntityFollowedEntities(args, callback) {
+        if (!args.profileName || !callback) {
+            throw new Error('Missing required params');
+        }
+
+        if ((typeof callback).toLowerCase() !== "function") {
+            throw new Error('Callback should be a function');
+        }
+
+        $.ajax({
+            type : "GET",
+            url :'/' + args.profileName + '/entitiesFollowed',
+            headers : {'csrf-token' : this.token},
+            success : function success(data) { callback(false, data); },
+            error : function error(err) { callback(true, err); }
+        });
+    },
+
     /* Creates a new Voice.
      * @argument args.data <required> [Object]
      * @argument callback <required> [Function]
