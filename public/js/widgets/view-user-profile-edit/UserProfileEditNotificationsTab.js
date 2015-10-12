fef4d96c9b2fbd7f9b06f0b29e5e787d69b2c11b
Noel Delgado <auhcsei@gmail.com>
2015-10-12 16:00:01 -0500

    add / manage profile, notifications mocked using the same format as specified on the wiki, so we expect the data we receive will be formatted as stated on the docs.

--- public/js/widgets/view-user-profile-edit/UserProfileEditNotificationsTab.js
+++ public/js/widgets/view-user-profile-edit/UserProfileEditNotificationsTab.js
@@ -1,20 +1,14 @@
-var NotifyMeData = require('./notifications/notify-me-data');
-var EmailDigestsData = require('./notifications/email-digests-data');
+var Settings = require('./notifications/data-notification-settings');
+var Labels = require('./notifications/data-notification-labels');
 
 Class(CV, 'UserProfileEditNotificationsTab').inherits(Widget)({
     HTML : '\
         <div>\
-            <div class="-mb3">\
+            <div class="-mb5">\
                 <div class="form-field -mb0">\
                     <label class="-p0">Notify Me</label>\
                 </div>\
                 <div data-notify-me-list></div>\
-            </div>\
-            <div class="-mb5">\
-                <div class="form-field -mb0">\
-                    <label class="-p0">Email Digests</label>\
-                </div>\
-                <div data-email-digests-list></div>\
             </div>\
         </div>',
 
@@ -22,36 +16,63 @@ Class(CV, 'UserProfileEditNotificationsTab').inherits(Widget)({
         init : function init(config) {
             Widget.prototype.init.call(this, config);
             this.el = this.element[0];
-            this.notifyMeList = this.el.querySelector('[data-notify-me-list]');
-            this.emailDigestsList = this.el.querySelector('[data-email-digests-list]');
 
             this._setup();
         },
 
         _setup : function _setup() {
+            var notifyMeListElement = this.el.querySelector('[data-notify-me-list]');
+            var formattedSettings = this._formatSettings(Settings);
+
+            Object.keys(formattedSettings).forEach(function(setting, index) {
+                this.appendChild(new CV.EditNotificationsNotifyMeItem({
+                    name : 'notify-me-item_' + index,
+                    className : 'cv-items-list',
+                    data : {
+                        label : Labels[setting] || 'missing description',
+                        options : formattedSettings[setting]
+                    }
+                })).render(notifyMeListElement);
+            }, this);
+
             this.appendChild(new CV.UI.Button({
                 name : 'saveButton',
                 className : 'positive small',
                 data : {value : 'Save Changes'},
             })).render(this.el);
 
-            NotifyMeData.forEach(function(item, index) {
-                this.appendChild(new CV.EditNotificationsNotifyMeItem({
-                    name : 'notify-me-item_' + index,
-                    className : 'cv-items-list',
-                    data : item
-                })).render(this.notifyMeList);
-            }, this);
-
-            EmailDigestsData.forEach(function(item, index) {
-                this.appendChild(new CV.EditNotificationsEmailDigestsItem({
-                    name : 'email-digests-item_' + index,
-                    className : 'cv-items-list',
-                    data : item
-                })).render(this.emailDigestsList);
-            }, this);
-
             return this;
+        },
+
+        /* Format notifications settings data to handle and display the interface.
+         * The output will be something like this for each setting.
+         *  {entityFollowsEntity :  [
+         *      {label: 'webSettings',   checked: true},
+         *      {label: 'emailSettings', checked: true }
+         *  ]}, ...
+         * @method _formatSettings <private>
+         * @return formatedData
+         */
+        _formatSettings : function _formatSettings(settings) {
+            var results = {};
+            var settingHumanLabel;
+
+            Object.keys(settings).forEach(function(settingsType) {
+                settingHumanLabel = (settingsType === 'webSettings') ? 'Web' : 'Email';
+
+                Object.keys(settings[settingsType]).forEach(function(settingType) {
+                    if (typeof results[settingType] === 'undefined') {
+                        results[settingType] = [];
+                    }
+
+                    results[settingType].push({
+                        settingsType: settingsType,
+                        label: settingHumanLabel,
+                        checked: settings[settingsType][settingType]
+                    });
+                });
+            });
+            return results;
         }
     }
 });
