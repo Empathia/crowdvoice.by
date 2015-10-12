ec46a2fda1aea07fbce81af21886bf1f15a9206f
greduan <me@greduan.com>
2015-10-11 23:26:50 -0500

    Add Featured Voices integration tests

--- test/integration/FeaturedVoicesController.js
+++ test/integration/FeaturedVoicesController.js
@@ -20,7 +20,7 @@ CONFIG.database.logQueries = false
 
 var urlBase = 'http://localhost:3000'
 
-test('Re-order featured people ( .updatePositions() )', function (t) {
+test('Re-order featured voices ( .updatePositions() )', function (t) {
   var cookies,
     csrf,
     agent = request.agent()
@@ -55,7 +55,7 @@ test('Re-order featured people ( .updatePositions() )', function (t) {
   ], function(err) {
     if (err) { t.fail(err) }
 
-    FeaturedPerson.all(function (err, featured) {
+    FeaturedVoice.all(function (err, featured) {
       if (err) { t.fail(err) }
 
       var hashedIds = featured.sort(function (a, b) {
@@ -65,74 +65,11 @@ test('Re-order featured people ( .updatePositions() )', function (t) {
       })
 
       agent
-        .post(urlBase + '/admin/featured/people/updatePositions')
+        .post(urlBase + '/admin/featured/voices/updatePositions')
         .accept('application/json')
         .send({
           _csrf: csrf,
-          entityIds: hashedIds,
-        })
-        .end(function (err, res) {
-          if (err) { t.fail(err) }
-
-          t.equal(res.status, 200, 'res.status')
-          t.equal(res.body.status, 'updated positions', 'res.body.status "updated positions"')
-
-          t.end()
-        })
-    })
-  })
-})
-
-test('Re-order featured organizations ( .updatePositions() )', function (t) {
-  var cookies,
-    csrf,
-    agent = request.agent()
-
-  async.series([
-    function (next) {
-      agent
-        .get(urlBase + '/csrf')
-        .end(function (err, res) {
-          if (err) { t.fail(err) }
-
-          csrf = res.text
-
-          return next()
-        })
-    },
-
-    function (next) {
-      agent
-        .post(urlBase + '/session')
-        .send({
-          _csrf: csrf,
-          username: 'cersei',
-          password: '12345678'
-        })
-        .end(function (err, res) {
-          if (err) { t.fail(err) }
-
-          return next()
-        })
-    },
-  ], function(err) {
-    if (err) { t.fail(err) }
-
-    FeaturedOrganization.all(function (err, featured) {
-      if (err) { t.fail(err) }
-
-      var hashedIds = featured.sort(function (a, b) {
-        return a.position - b.position
-      }).map(function (val) {
-        return hashids.encode(val.entityId)
-      })
-
-      agent
-        .post(urlBase + '/admin/featured/organizations/updatePositions')
-        .accept('application/json')
-        .send({
-          _csrf: csrf,
-          entityIds: hashedIds,
+          voiceIds: hashedIds,
         })
         .end(function (err, res) {
           if (err) { t.fail(err) }
