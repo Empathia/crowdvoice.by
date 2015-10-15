f1177e302d8cd92da28d3eaa6297487d5ec05fed
greduan <me@greduan.com>
2015-10-15 17:10:56 -0500

    Convert to Mocha FeaturedEntitiesController tests

--- test/integration/FeaturedEntitiesController.js
+++ test/integration/FeaturedEntitiesController.js
@@ -1,5 +1,7 @@
 'use strict'
 
+// SETUP NEONODE
+
 global.Admin = {}
 
 var application = require('neonode-core')
@@ -13,16 +15,18 @@ require(path.join(__dirname, '../../presenters/PostsPresenter'))
 
 application._serverStart()
 
-var request = require('superagent')
-var test = require('tape-catch')
+// COMMENT IF YOU WANT LOGGER OUTPUT
+logger.log = function () {}
+
+var request = require('superagent'),
+  expect = require('chai').expect
 
 CONFIG.database.logQueries = false
 
 var urlBase = 'http://localhost:3000'
 
-test('Re-order featured people ( .updatePositions() )', function (t) {
-  var cookies,
-    csrf,
+var login = function (username, callback) {
+  var csrf,
     agent = request.agent()
 
   async.series([
@@ -30,7 +34,7 @@ test('Re-order featured people ( .updatePositions() )', function (t) {
       agent
         .get(urlBase + '/csrf')
         .end(function (err, res) {
-          if (err) { t.fail(err) }
+          if (err) { return callback(err) }
 
           csrf = res.text
 
@@ -43,163 +47,130 @@ test('Re-order featured people ( .updatePositions() )', function (t) {
         .post(urlBase + '/session')
         .send({
           _csrf: csrf,
-          username: 'cersei',
+          username: username,
           password: '12345678'
         })
         .end(function (err, res) {
-          if (err) { t.fail(err) }
+          if (err) { return callback(err) }
 
           return next()
         })
     },
-  ], function(err) {
-    if (err) { t.fail(err) }
+  ], function (err) {
+    if (err) { return callback(err) }
 
-    FeaturedPerson.all(function (err, featured) {
-      if (err) { t.fail(err) }
+    return callback(null, agent, csrf)
+  })
+}
 
-      var hashedIds = featured.sort(function (a, b) {
-        return a.position - b.position
-      }).map(function (val) {
-        return hashids.encode(val.entityId)
-      })
+// ACTUAL TESTS
 
-      agent
-        .post(urlBase + '/admin/featured/people/updatePositions')
-        .accept('application/json')
-        .send({
-          _csrf: csrf,
-          entityIds: hashedIds,
-        })
-        .end(function (err, res) {
+describe('FeaturedEntitiesController', function () {
+
+  describe('#updatePositions', function () {
+
+    it('Re-order featured people', function (done) {
+      // LOGIN
+      login('cersei', function (err, agent, csrf) {
+        if (err) { return done(err) }
+
+        FeaturedPerson.all(function (err, featured) {
           if (err) { t.fail(err) }
 
-          t.equal(res.status, 200, 'res.status')
-          t.equal(res.body.status, 'updated positions', 'res.body.status "updated positions"')
+          var hashedIds = featured.sort(function (a, b) {
+            return a.position - b.position
+          }).map(function (val) {
+            return hashids.encode(val.entityId)
+          })
 
-          t.end()
+          // REORDER
+          agent
+            .post(urlBase + '/admin/featured/people/updatePositions')
+            .accept('application/json')
+            .send({
+              _csrf: csrf,
+              entityIds: hashedIds,
+            })
+            .end(function (err, res) {
+              if (err) { return done(err) }
+
+              expect(res.status).to.equal(200)
+              expect(res.body.status).to.equal('updated positions')
+
+              done()
+            })
         })
+      })
     })
-  })
-})
 
-test('Re-order featured organizations ( .updatePositions() )', function (t) {
-  var cookies,
-    csrf,
-    agent = request.agent()
+    it('Re-order featured organizations', function (done) {
+      // LOGIN
+      login('cersei', function (err, agent, csrf) {
+        if (err) { return done(err) }
 
-  async.series([
-    function (next) {
-      agent
-        .get(urlBase + '/csrf')
-        .end(function (err, res) {
+        FeaturedOrganization.all(function (err, featured) {
           if (err) { t.fail(err) }
 
-          csrf = res.text
+          var hashedIds = featured.sort(function (a, b) {
+            return a.position - b.position
+          }).map(function (val) {
+            return hashids.encode(val.entityId)
+          })
 
-          return next()
+          // REORDER
+          agent
+            .post(urlBase + '/admin/featured/organizations/updatePositions')
+            .accept('application/json')
+            .send({
+              _csrf: csrf,
+              entityIds: hashedIds,
+            })
+            .end(function (err, res) {
+              if (err) { return done(err) }
+
+              expect(res.status).to.equal(200)
+              expect(res.body.status).to.equal('updated positions')
+
+              done()
+            })
         })
-    },
-
-    function (next) {
-      agent
-        .post(urlBase + '/session')
-        .send({
-          _csrf: csrf,
-          username: 'cersei',
-          password: '12345678'
-        })
-        .end(function (err, res) {
-          if (err) { t.fail(err) }
-
-          return next()
-        })
-    },
-  ], function(err) {
-    if (err) { t.fail(err) }
-
-    FeaturedOrganization.all(function (err, featured) {
-      if (err) { t.fail(err) }
-
-      var hashedIds = featured.sort(function (a, b) {
-        return a.position - b.position
-      }).map(function (val) {
-        return hashids.encode(val.entityId)
       })
-
-      agent
-        .post(urlBase + '/admin/featured/organizations/updatePositions')
-        .accept('application/json')
-        .send({
-          _csrf: csrf,
-          entityIds: hashedIds,
-        })
-        .end(function (err, res) {
-          if (err) { t.fail(err) }
-
-          t.equal(res.status, 200, 'res.status')
-          t.equal(res.body.status, 'updated positions', 'res.body.status "updated positions"')
-
-          t.end()
-        })
     })
+
   })
-})
 
-test('Create new featured organization ( .create() )', function (t) {
-  var cookies,
-    csrf,
-    agent = request.agent()
+  describe('#create', function () {
 
-  async.series([
-    function (next) {
-      agent
-        .get(urlBase + '/csrf')
-        .end(function (err, res) {
-          if (err) { t.fail(err) }
+    it('Add featured organization', function (done) {
+      // LOGIN
+      login('cersei', function (err, agent, csrf) {
+        if (err) { return done(err) }
 
-          csrf = res.text
+        // CREATE
+        agent
+          .post(urlBase + '/admin/featured/organizations/new')
+          .accept('application/json')
+          .send({
+            _csrf: csrf,
+            entityId: 'AVolB9X1b3Ym', // 24, House Baratheon
+          })
+          .end(function (err, res) {
+            if (err) { return done(err) }
 
-          return next()
-        })
-    },
+            expect(res.status).to.equal(200)
 
-    function (next) {
-      agent
-        .post(urlBase + '/session')
-        .send({
-          _csrf: csrf,
-          username: 'cersei',
-          password: '12345678'
-        })
-        .end(function (err, res) {
-          if (err) { t.fail(err) }
+            FeaturedOrganization.find({ entity_id: 24 }, function (err, result) {
+              if (err) { return done(err) }
 
-          return next()
-        })
-    },
-  ], function(err) {
-    if (err) { t.fail(err) }
+              expect(result.length).to.equal(1)
 
-    agent
-      .post(urlBase + '/admin/featured/organizations/new')
-      .accept('application/json')
-      .send({
-        _csrf: csrf,
-        entityId: 'AVolB9X1b3Ym', // House Baratheon
+              done()
+            })
+
+          })
       })
-      .end(function (err, res) {
-        if (err) { console.log(err); t.fail(err) }
+    })
 
-        t.equal(res.status, 200, 'res.status')
-
-        FeaturedOrganization.find({ entity_id: 24 }, function (err, result) {
-          if (err) { return t.fail(err) }
-
-          t.equal(result.length, 1, 'one record in the DB')
-
-          t.end()
-        })
-      })
   })
+
 })
