dc6bb275cb0fcde1bf53fa660ae840deb881d82a
greduan <me@greduan.com>
2015-10-28 17:11:32 -0600

    Fix #563, add .voicesCount prop to TopicsPresenter

--- presenters/TopicsPresenter.js
+++ presenters/TopicsPresenter.js
@@ -11,24 +11,46 @@ var TopicsPresenter = Module('TopicsPresenter')({
         return nextTopic();
       }
 
-      // Images
-      var images = {};
+      async.series([
+        // Images
+        function (next) {
+          var images = {};
 
-      for (var version in topicInstance.imageMeta) {
-        images[version] = {
-          url : topicInstance.image.url(version),
-          meta : topicInstance.image.meta(version)
-        }
-      }
+          for (var version in topicInstance.imageMeta) {
+            images[version] = {
+              url : topicInstance.image.url(version),
+              meta : topicInstance.image.meta(version)
+            }
+          }
 
-      topicInstance.images = images;
+          topicInstance.images = images;
 
-      delete topicInstance.imageBaseUrl;
-      delete topicInstance.imageMeta;
+          delete topicInstance.imageBaseUrl;
+          delete topicInstance.imageMeta;
 
-      response.push(topicInstance);
+          return next();
+        },
 
-      nextTopic();
+        // Voices count
+        function (next) {
+          db('VoiceTopic')
+            .count()
+            .where('topic_id', '=', topic.id)
+            .exec(function (err, result) {
+              if (err) { return next(err); }
+
+              topicInstance.voicesCount = result[0].count
+
+              return next();
+            });
+        },
+      ], function (err) {
+        if (err) { return nextTopic(err); }
+
+        response.push(topicInstance);
+
+        return nextTopic();
+      });
     }, function(err) {
       if (err) {
         return callback(err);
