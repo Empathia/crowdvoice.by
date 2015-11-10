70d3f5d4dd87c72c8a85af8a450c5e3ed082432c
Noel Delgado <auhcsei@gmail.com>
2015-11-10 05:23:59 -0600

    add / empty state: browse: featured people

--- public/js/widgets/view-discover-browse/DiscoverBrowsePeopleTab.js
+++ public/js/widgets/view-discover-browse/DiscoverBrowsePeopleTab.js
@@ -47,7 +47,12 @@ Class(CV, 'DiscoverBrowsePeopleTab').inherits(Widget)({
             }
 
             this._fetched = true;
-            this._renderResults(res);
+
+            if (res.length) {
+                return this._renderResults(res);
+            }
+
+            return this._renderEmptyState();
         },
 
         /* Render the getBrowseFeaturedPeople response EntityCards.
@@ -71,6 +76,16 @@ Class(CV, 'DiscoverBrowsePeopleTab').inherits(Widget)({
             }).setup();
 
             this.el.appendChild(fragment);
+
+            this.loader.disable();
+        },
+
+        _renderEmptyState : function _renderEmptyState() {
+            this.appendChild(new CV.EmptyState({
+                name : 'empty',
+                className : '-pt4 -pb4',
+                message : 'there are no featured people yet.'
+            })).render(this.el);
 
             this.loader.disable();
         },
