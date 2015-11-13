fcae37f091a71320d9b6e147887f2a2f57f93d75
Noel Delgado <auhcsei@gmail.com>
2015-11-12 18:14:39 -0600

    update / edit voice, required cover message

--- public/js/widgets/forms/CreateVoice.js
+++ public/js/widgets/forms/CreateVoice.js
@@ -136,7 +136,7 @@ Class(CV, 'CreateVoice').inherits(Widget).includes(CV.WidgetUtils)({
 
             this.appendChild(new CV.Image({
                 name : 'voiceImage',
-                data: {title : "Background image"}
+                data: {title : "Thumbnail image"}
             })).render(this.el.querySelector('[data-background]'));
 
             this.appendChild(new CV.UI.Input({
@@ -567,7 +567,7 @@ Class(CV, 'CreateVoice').inherits(Widget).includes(CV.WidgetUtils)({
             }, this);
 
             if (this.voiceImage.hasError()) {
-                var message = 'Custom message telling voice background is required (to be replaced).';
+                var message = 'Please upload a thumbnail image to your voice, this will be the face of your voice.';
 
                 if (this._flashMessage) {
                     return this._flashMessage.update({
