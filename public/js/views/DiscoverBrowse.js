90f3b51ccdee33324c986375940ada8696ed7222
AnchoWake <ren.suga@gmail.com>
2015-11-26 13:55:17 -0600

    Fix Ticket720
    Discover/Browse buttons(trending,newest and recommended) are now using the voice card hover animation, and getting their background image from voices in their topic.
    
    Trending, newest and recommended buttons new hover animation
    
    Trending, newest and recommended buttons now display a image from a voice tagged in their category
    
    Signed-off-by: Sergio de la Garza <sergio@delagarza.io>

--- public/js/views/DiscoverBrowse.js
+++ public/js/views/DiscoverBrowse.js
@@ -1,14 +1,23 @@
+var API = require('./../lib/api');
+
 Class(CV.Views, 'DiscoverBrowse').includes(NodeSupport, CV.WidgetUtils)({
     prototype : {
         /* Existing Topics.
          * @property topicsData <required> [Array] TopicsPresenter
          */
         topicsData : null,
+        trendingImage : null,
+        newestImage : null,
+        recommendedImage : null,
 
         init : function init(config) {
             Object.keys(config || {}).forEach(function(propertyName) {
                 this[propertyName] = config[propertyName];
             }, this);
+
+            this._requestTrendingImage();
+            this._requestNewestImage();
+            this._requestRecommendedImage();
 
             this._setup();
         },
@@ -48,13 +57,48 @@ Class(CV.Views, 'DiscoverBrowse').includes(NodeSupport, CV.WidgetUtils)({
                     className: '-float-left',
                     data: topic
                 })).render(topicsHolder).el);
-            }, this);
+            }, this);   
 
             new CV.ResponsiveWidth({
                 container : topicsHolder,
                 items : [].slice.call(topicsElements, 0),
                 minWidth : 300
             }).setup();
+
+        },
+
+        _requestTrendingImage : function _requestTrendingImage() {
+            API.getTrendingVoices(function (err, res){
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                this.trendingImage = document.querySelector('.browse-element-button-trending');
+                this.trendingImage.querySelector('.browse-bg-image').style.backgroundImage = "url('"+ res[0].images.card.url +"')";
+            });
+        },
+
+        _requestNewestImage : function _requestNewestImage() {
+            API.getNewVoices(function (err, res){
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                this.newestImage = document.querySelector('.browse-element-button-newest');
+                this.newestImage.querySelector('.browse-bg-image').style.backgroundImage = "url('"+ res[0].images.card.url +"')";
+            });
+        },
+
+        _requestRecommendedImage : function _requestRecommendedImage(){
+            API.getBrowseFeaturedVoices(function (err, res){
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                
+                this.recommendedImage = document.querySelector('.browse-element-button-recommended');
+                this.recommendedImage.querySelector('.browse-bg-image').style.backgroundImage = "url('"+ res[0].images.card.url +"')";
+            });
         }
     }
 });
