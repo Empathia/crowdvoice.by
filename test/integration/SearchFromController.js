c2a783efbc380b6f5260af2e82332c7c90ec9d14
greduan <me@greduan.com>
2015-11-26 22:30:02 -0600

    Fix 713, anonymous route crash when % in URL
    This basically meant almost anything encoded with encodeURIComponent
    would just never find a route.
    
    Signed-off-by: Sergio de la Garza <sergio@delagarza.io>

---
+++ test/integration/SearchFromController.js
@@ -0,0 +1,46 @@
+'use strict'
+
+global.Admin = {}
+
+var application = require('neonode-core')
+require(path.join(__dirname, '../../lib/routes.js'))
+
+// Load moment
+global.moment = require('moment')
+
+global.FeedInjector = require(path.join(__dirname, '../../lib/FeedInjector.js'))
+require(path.join(__dirname, '../../presenters/PostsPresenter'))
+
+application._serverStart()
+
+// COMMENT IF YOU WANT LOGGER OUTPUT
+logger.log = function () {}
+
+var login = require(path.join(__dirname, 'login.js')),
+  expect = require('chai').expect,
+  request = require('superagent')
+
+CONFIG.database.logQueries = false
+
+var urlBase = 'http://localhost:3000'
+
+describe('SearchFromController', function () {
+
+  describe('#youtube', function () {
+
+    it('Should not crash on anonymous voices when search term has a space', function (doneTest) {
+      request
+        .get(urlBase + '/anonymous_' + hashids.encode(2) + '/second-trial-by-combat/youtube/' + encodeURIComponent('pokemon rocks'))
+        .accept('application/json')
+        .end(function (err, res) {
+          if (err) { return doneTest(err) }
+
+          expect(res.status).to.equal(200)
+
+          return doneTest()
+        })
+    })
+
+  })
+
+})
