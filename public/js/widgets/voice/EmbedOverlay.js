3a19bc106e8287b6816317bccef2f45117c33611
AnchoWake <ren.suga@gmail.com>
2015-12-04 11:30:47 -0600

    Changed from modal to overlay

--- public/js/widgets/voice/EmbedOverlay.js
+++ public/js/widgets/voice/EmbedOverlay.js
@@ -1,53 +1,47 @@
 Class(CV.UI, 'EmbedOverlay').inherits(Widget)({
-  ELEMENT_CLASS : 'cv-embed-modal-container ui-modal',
-
   HTML: '\
-    <div>\
-      <div class="cv-modal__backdrop"></div>\
-      <div class="cv-modal__inner">\
-        <div class="cv-embed-modal">\
-          <div class="cv-embed-modal__iframe">\
-            <div class="cv-embed-iframe-wraper">\
-            </div>\
-          </div>\
-          <div class="cv-embed-modal__options">\
-            <p class="embed-title">Widget Settings</p>\
-            <div class="cv-embed-modal__height">\
-              <p>Widget Height:</p>\
-            </div>\
-            <div class="cv-embed-modal__view">\
-              <p>Default view:</p>\
-            </div>\
-            <div class="cv-embed-modal__theme">\
-              <p>Theme</p>\
-            </div>\
-            <div class="cv-embed-modal__accent">\
-              <p>Pick Accent Color</p>\
-            </div>\
-            <div class="cv-embed-modal__share"></div>\
-            <div class="cv-embed-modal__code"></div>\
+    <div class="cv-embed-overlay__background"></div>\
+    <div class="cv-embed-overlay__body">\
+        <div class="cv-embed-overlay__iframe">\
+          <div class="cv-embed-iframe-wraper">\
           </div>\
         </div>\
-      </div>\
+        <div class="cv-embed-overlay__options">\
+          <p class="cv-embed-overlay__title">Widget Settings</p>\
+          <div class="cv-embed-overlay__height">\
+            <p>Widget Height:</p>\
+          </div>\
+          <div class="cv-embed-overlay__view">\
+            <p>Default view:</p>\
+          </div>\
+          <div class="cv-embed-overlay__theme">\
+            <p>Theme</p>\
+          </div>\
+          <div class="cv-embed-overlay__accent">\
+            <p>Pick Accent Color</p>\
+          </div>\
+          <div class="cv-embed-overlay__share"></div>\
+          <div class="cv-embed-overlay__code"></div>\
+        </div>\
     </div>\
   ',
 
   prototype : {
     init : function(config) {
       Widget.prototype.init.call(this, config);
+      
+      this.el = this.element[1];
 
-      this.el = this.element[0];
-
-      this.embedWidgetContainer = this.el.querySelector('.cv-embed-modal__iframe');
+      this.embedWidgetContainer = this.el.querySelector('.cv-embed-overlay__iframe');
       this.iframeInner = this.el.querySelector('.cv-embed-iframe-wraper');
 
-      this.optionsContainer = this.el.querySelector('.cv-embed-modal__options');
-      this.optionHeight = this.optionsContainer.querySelector('.cv-embed-modal__height');
-      this.optionView = this.optionsContainer.querySelector('.cv-embed-modal__view');
-      this.optionTheme = this.optionsContainer.querySelector('.cv-embed-modal__theme');
-      this.optionAccent = this.optionsContainer.querySelector('.cv-embed-modal__accent');
-      this.optionShare = this.optionsContainer.querySelector('.cv-embed-modal__share');
-      this.optionCode = this.optionsContainer.querySelector('.cv-embed-modal__code');
+      this.optionsContainer = this.el.querySelector('.cv-embed-overlay__options');
+      this.optionHeight = this.optionsContainer.querySelector('.cv-embed-overlay__height');
+      this.optionView = this.optionsContainer.querySelector('.cv-embed-overlay__view');
+      this.optionTheme = this.optionsContainer.querySelector('.cv-embed-overlay__theme');
+      this.optionAccent = this.optionsContainer.querySelector('.cv-embed-overlay__accent');
+      this.optionShare = this.optionsContainer.querySelector('.cv-embed-overlay__share');
+      this.optionCode = this.optionsContainer.querySelector('.cv-embed-overlay__code');
 
       this._setup()._bindEvents();
     },
@@ -56,7 +50,7 @@ Class(CV.UI, 'EmbedOverlay').inherits(Widget)({
       var voiceIframe = document.createElement('iframe');
       var iframeAdvice = document.createElement('p');
 
-      voiceIframe.setAttribute('src', 'http://localhost:3000/embed/cersei-lannister/walk-of-atonement/?default_view=cards&change_view=true&description=false&background=true&share=true&theme=dark&accent=4DD5B9');
+      voiceIframe.setAttribute('src', 'http://localhost:3000/embed/cersei-lannister/walk-of-atonement/?default_view=cards&change_view=true&description=true&background=true&share=true&theme=dark&accent=4DD5B9');
       iframeAdvice.innerHTML = '<i>Widget Width is set to 100% (min width 320 px)</i>';
       this.iframeInner.appendChild(voiceIframe);
       this.embedWidgetContainer.appendChild(iframeAdvice);
@@ -65,7 +59,7 @@ Class(CV.UI, 'EmbedOverlay').inherits(Widget)({
         name : 'closeButton',
         className : '-clickable -color-white -abs',
         svgClassName : '-s18'
-      })).render(this.el.querySelector('.cv-modal__inner'));
+      })).render(this.el);
 
       /* 
        * Widget Height Radios
