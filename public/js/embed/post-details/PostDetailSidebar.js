625a31fa95c3ac1a8d5bd1213caf1cd4bed6c1b5
Noel Delgado <auhcsei@gmail.com>
2015-12-06 21:40:56 -0600

    add / content viewer:responsive:card view
    - added content viewer card style when under 551 viewport width

--- public/js/embed/post-details/PostDetailSidebar.js
+++ public/js/embed/post-details/PostDetailSidebar.js
@@ -4,153 +4,146 @@ var ScrollTo = require('./../../lib/scrollto');
 Class(CV, 'PostDetailSidebar').inherits(Widget).includes(BubblingSupport)({
   ELEMENT_CLASS : 'cv-post-detail__sidebar -full-height',
   HTML : '\
-      <div>\
-        <div class="cv-post-detail__sidebar-filter"></div>\
-        <div class="cv-post-detail__sidebar-scrollbar">\
-          <div class="gm-scrollbar -vertical">\
-            <div class="thumb"></div>\
-          </div>\
-          <div class="gm-scrollbar -horizontal">\
-            <div class="thumb"></div>\
-          </div>\
-          <div class="gm-scroll-view"></div>\
+    <div>\
+      <div class="cv-post-detail__sidebar-filter"></div>\
+      <div class="cv-post-detail__sidebar-scrollbar">\
+        <div class="gm-scrollbar -vertical">\
+          <div class="thumb"></div>\
         </div>\
-      </div>',
+        <div class="gm-scrollbar -horizontal">\
+          <div class="thumb"></div>\
+        </div>\
+        <div class="gm-scroll-view"></div>\
+      </div>\
+    </div>',
 
-    prototype : {
-      _activatedItem : null,
+  prototype : {
+    _activatedItem : null,
 
-      init : function init (config) {
-        Widget.prototype.init.call(this, config);
-        this.el = this.element[0];
+    init : function init(config) {
+      Widget.prototype.init.call(this, config);
+      this.el = this.element[0];
 
-        this.filterDropdown = new CV.VoiceFilterPostsDropdown({
-          className : '-rel',
-          dropdownClassName : 'dark'
-        }).render(this.el.querySelector('.cv-post-detail__sidebar-filter'));
+      this.filterDropdown = new CV.VoiceFilterPostsDropdown({
+        className : '-rel',
+        dropdownClassName : 'dark'
+      }).render(this.el.querySelector('.cv-post-detail__sidebar-filter'));
 
-        this.scrollbar = new GeminiScrollbar({
-          element: this.el.querySelector('.cv-post-detail__sidebar-scrollbar'),
-          createElements: false,
-          autoshow: true
-        }).create();
+      this.scrollbar = new GeminiScrollbar({
+        element: this.el.querySelector('.cv-post-detail__sidebar-scrollbar'),
+        createElements: false,
+        autoshow: true
+      }).create();
 
-        this._bindEvents();
-      },
+      this._bindEvents();
+    },
 
-      _bindEvents : function _bindEvents () {
-        this._renderHandlerRef = this._renderHandler.bind(this);
-        this.bind('render', this._renderHandlerRef);
-        this.bind('deactivate', this._bubbledDeactivate);
+    _bindEvents : function _bindEvents() {
+      this._renderHandlerRef = this._renderHandler.bind(this);
+      this.bind('render', this._renderHandlerRef);
+      this.bind('deactivate', this._bubbledDeactivate);
 
-        this._filterSelectionUpdatedRef = this._filterSelectionUpdated.bind(this);
-        this.filterDropdown.bind('selectionUpdated', this._filterSelectionUpdatedRef);
-        return this;
-      },
+      this._filterSelectionUpdatedRef = this._filterSelectionUpdated.bind(this);
+      this.filterDropdown.bind('selectionUpdated', this._filterSelectionUpdatedRef);
+      return this;
+    },
 
-      /* Destroys any sidebar children and creates them using the new posts
-       * array passed. This allow us to keep the order of thumbs intact and
-       * updated everytime we get new posts fetched from the server.
-       * @public
-       * @param {Array} posts - the new fetched posts to add to the sidebar
-       */
-      updateItems : function updateItems (posts) {
-        while(this.children.length > 0) {
-          this.children[0].destroy();
-        }
+    /* Destroys any sidebar children and creates them using the new posts
+     * array passed. This allow us to keep the order of thumbs intact and
+     * updated everytime we get new posts fetched from the server.
+     * @public
+     * @param {Array} posts - the new fetched posts to add to the sidebar
+     */
+    updateItems : function updateItems (posts) {
+      while(this.children.length > 0) {
+        this.children[0].destroy();
+      }
 
-        posts.forEach(function(post) {
-          this.appendChild(new CV.PostDetailSidebarItem({
-            name : 'item_' + post.id,
-            data : post
-          })).render(this.scrollbar.getViewElement());
-        }, this);
+      posts.forEach(function(post) {
+        this.appendChild(new CV.PostDetailSidebarItem({
+          name : 'item_' + post.id,
+          data : post
+        })).render(this.scrollbar.getViewElement());
+      }, this);
 
-        this.filterItems(this.filterDropdown.getSelectedSourceTypes());
+      this.filterItems(this.filterDropdown.getSelectedSourceTypes());
 
-        this.scrollbar.update();
-      },
+      this.scrollbar.update();
+    },
 
-      /* Iterate over all items and deactivate them, activates the one passed
-       * as currently active and scrollsInto the activate thumb.
-       * @public
-       * @param {Object} item - the current active PostInstance
-       */
-      activateItem : function activateItem (item) {
-        this._activatedItem = null;
+    /* Iterate over all items and deactivate them, activates the one passed
+     * as currently active and scrollsInto the activate thumb.
+     * @public
+     * @param {Object} item - the current active PostInstance
+     */
+    activateItem : function activateItem (item) {
+      this._activatedItem = null;
 
-        this.children.forEach(function(post) {
-          if (post.data === item) {
-            this._activatedItem = post.activate();
-          } else {
-            post.deactivate();
-          }
-        }, this);
+      this.children.forEach(function(post) {
+        if (post.data === item) {
+          this._activatedItem = post.activate();
+        } else { post.deactivate(); }
+      }, this);
 
-        if (this._activatedItem) {
-          ScrollTo(this.scrollbar.getViewElement(), {
-            x: 0,
-            y: (this._activatedItem.el.getBoundingClientRect().top - 83),
-            duration: 600
-          });
-        }
-      },
+      if (this._activatedItem) {
+        ScrollTo(this.scrollbar.getViewElement(), {
+          x: 0,
+          y: (this._activatedItem.el.getBoundingClientRect().top - 83),
+          duration: 600
+        });
+      }
+    },
 
-      filterItems : function filterItems (sourceTypes) {
-        var showAll = false;
+    filterItems : function filterItems (sourceTypes) {
+      var showAll = false;
 
-        if (!sourceTypes) {
-          showAll = true;
-        }
+      if (!sourceTypes) { showAll = true; }
 
-        function showAllFn(post) {
+      function showAllFn(post) {
+        post.el.style.display = 'block';
+      }
+
+      function filterFn(post) {
+        if (sourceTypes.indexOf(post.data.sourceType) < 0) {
+          post.el.style.display = 'none';
+        } else {
           post.el.style.display = 'block';
         }
-
-        function filterFn(post) {
-          if (sourceTypes.indexOf(post.data.sourceType) < 0) {
-            post.el.style.display = 'none';
-          } else {
-            post.el.style.display = 'block';
-          }
-        }
-
-        if (showAll) {
-          this.children.forEach(showAllFn);
-        } else {
-          this.children.forEach(filterFn);
-        }
-      },
-
-      /* Render event handler.
-       * @private
-       */
-      _renderHandler : function _renderHandler () {
-        setTimeout(function (that) {
-          that.scrollbar.update();
-        }, 0, this);
-      },
-
-      /* Prevent the `deactiveate` event to be bubbled up.
-       * @private
-       */
-      _bubbledDeactivate : function _bubbledDeactivate (ev) {
-        ev.stopPropagation();
-      },
-
-      _filterSelectionUpdated : function _filterSelectionUpdated (ev) {
-        this.filterItems(ev.sourceTypes);
-      },
-
-      destroy : function destroy () {
-        this.unbind('deactivate', this._bubbledDeactivate);
-        this.scrollbar = this.scrollbar.destroy();
-
-        this.filterDropdown.unbind('selectionUpdated', this._filterSelectionUpdatedRef);
-        this._filterSelectionUpdatedRef = null;
-
-        Widget.prototype.destroy.call(this);
-        return null;
       }
+
+      if (showAll) { this.children.forEach(showAllFn); }
+      else { this.children.forEach(filterFn); }
+    },
+
+    /* Render event handler.
+     * @private
+     */
+    _renderHandler : function _renderHandler () {
+      window.setTimeout(function (that) {
+        that.scrollbar.update();
+      }, 0, this);
+    },
+
+    /* Prevent the `deactiveate` event to be bubbled up.
+     * @private
+     */
+    _bubbledDeactivate : function _bubbledDeactivate (ev) {
+      ev.stopPropagation();
+    },
+
+    _filterSelectionUpdated : function _filterSelectionUpdated (ev) {
+      this.filterItems(ev.sourceTypes);
+    },
+
+    destroy : function destroy () {
+      this.unbind('deactivate', this._bubbledDeactivate);
+      this.scrollbar = this.scrollbar.destroy();
+
+      this.filterDropdown.unbind('selectionUpdated', this._filterSelectionUpdatedRef);
+      this._filterSelectionUpdatedRef = null;
+
+      Widget.prototype.destroy.call(this);
+      return null;
     }
+  }
 });
--- public/js/embed/post-details/PostDetailTimeline.js
+++ public/js/embed/post-details/PostDetailTimeline.js
@@ -2,68 +2,69 @@ var moment = require('moment');
 var Events = require('./../../lib/events');
 
 Class(CV, 'PostDetailTimeline').inherits(Widget)({
-    HTML : '\
-        <div class="cv-post-detail-timeline-wrapper">\
-            <div class="cv-post-detail-timeline-feedback"></div>\
-        </div>',
-    prototype : {
-        firstPostDate : '',
-        lastPostDate : '',
+  HTML : '\
+    <div class="cv-post-detail-timeline-wrapper">\
+      <div class="cv-post-detail-timeline-feedback"></div>\
+    </div>',
 
-        _currentPost : null,
-        _clientWidth : 0,
+  prototype: {
+    firstPostDate : '',
+    lastPostDate : '',
 
-        /* @param {string} config.firstPostDate - voice’s first post timestamp
-         * @param {string} config.lastPostDate - voice’s last post timestamp
-         */
-        init: function init(config) {
-            Widget.prototype.init.call(this, config);
-            this.el = this.element[0];
-            this.feedback = this.el.querySelector('.cv-post-detail-timeline-feedback');
+    _currentPost : null,
+    _clientWidth : 0,
 
-            this._firstDateMS = moment(this.firstPostDate).format('x') * 1000;
-            this._lastDateMS = moment(this.lastPostDate).format('x') * 1000;
+    /* @param {string} config.firstPostDate - voice’s first post timestamp
+     * @param {string} config.lastPostDate - voice’s last post timestamp
+     */
+    init: function init(config) {
+      Widget.prototype.init.call(this, config);
+      this.el = this.element[0];
+      this.feedback = this.el.querySelector('.cv-post-detail-timeline-feedback');
 
-            this._updateVars()._bindEvents();
-        },
+      this._firstDateMS = moment(this.firstPostDate).format('x') * 1000;
+      this._lastDateMS = moment(this.lastPostDate).format('x') * 1000;
 
-        /* Update the feedback indicator position using the Post.publishedAt value.
-         * @public
-         * @param {Object} post - current active post on the detail view
-         */
-        update : function update(post) {
-            var currentDateMs = moment(post.publishedAt).format('x') * 1000;
-            var percent = (100 * (currentDateMs - this._firstDateMS)) / (this._lastDateMS - this._firstDateMS);
-            var pixels = ~~(this._clientWidth * percent / 100);
-            var transform = 'translateX(' + pixels + 'px)';
+      this._updateVars()._bindEvents();
+    },
 
-            this.feedback.style.msTransform = transform;
-            this.feedback.style.webkitTransform = transform;
-            this.feedback.style.transform = transform;
-            this._currentPost = post;
-        },
+    /* Update the feedback indicator position using the Post.publishedAt value.
+     * @public
+     * @param {Object} post - current active post on the detail view
+     */
+    update : function update(post) {
+      var currentDateMs = moment(post.publishedAt).format('x') * 1000;
+      var percent = (100 * (currentDateMs - this._firstDateMS)) / (this._lastDateMS - this._firstDateMS);
+      var pixels = ~~(this._clientWidth * percent / 100);
+      var transform = 'translateX(' + pixels + 'px)';
 
-        _bindEvents : function _bindEvents() {
-            this._updateVarsRef = this._updateVars.bind(this);
-            Events.on(window, 'resize', this._updateVarsRef);
-            return this;
-        },
+      this.feedback.style.msTransform = transform;
+      this.feedback.style.webkitTransform = transform;
+      this.feedback.style.transform = transform;
+      this._currentPost = post;
+    },
 
-        _updateVars : function _updateVars() {
-            this._clientWidth = (window.innerWidth - 15);
+    _bindEvents : function _bindEvents() {
+      this._updateVarsRef = this._updateVars.bind(this);
+      Events.on(window, 'resize', this._updateVarsRef);
+      return this;
+    },
 
-            if (this._currentPost) {
-                this.update(this._currentPost);
-            }
+    _updateVars : function _updateVars() {
+      this._clientWidth = (window.innerWidth - 15);
 
-            return this;
-        },
+      if (this._currentPost) {
+        this.update(this._currentPost);
+      }
 
-        destroy : function destroy() {
-            Widget.prototype.destroy.call(this);
-            Events.off(window, 'resize', this._updateVarsRef);
-            this._updateVarsRef = null;
-            return null;
-        }
+      return this;
+    },
+
+    destroy : function destroy() {
+      Widget.prototype.destroy.call(this);
+      Events.off(window, 'resize', this._updateVarsRef);
+      this._updateVarsRef = null;
+      return null;
     }
+  }
 });
