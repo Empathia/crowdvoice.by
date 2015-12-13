51e785f5a5dbbb2036668f5c06d02927d8953755
Noel Delgado <auhcsei@gmail.com>
2015-12-12 21:42:07 -0600

    update / reindent Feed.js to 2 spaces

--- public/js/views/Feed.js
+++ public/js/views/Feed.js
@@ -1,75 +1,81 @@
 var Person = require('./../lib/currentPerson');
 
 Class(CV.Views, 'Feed').includes(NodeSupport, CV.WidgetUtils)({
-    prototype : {
-        organization: null,
-        feedItems : null,
+  prototype: {
+    organization: null,
+    feedItems: null,
+    topVoice: null,
 
-        init : function init(config) {
-            Object.keys(config || {}).forEach(function(propertyName) {
-                this[propertyName] = config[propertyName];
-            }, this);
+    init: function init(config) {
+      Object.keys(config || {}).forEach(function(propertyName) {
+        this[propertyName] = config[propertyName];
+      }, this);
 
-            this._setup();
-        },
+      this._setup();
+    },
 
-        _setup : function _setup() {
-            if (Person.ownsOrganizations()) {
-                var currentEntityView = Person.get();
+    _setup: function _setup() {
+      this.appendChild(new CV.TopVoice({
+        name: 'topVoice',
+        data: this.topVoice
+      })).render(this.el.querySelector('.feed-top-voice'));
 
-                this.appendChild(new CV.UI.FeedDropdown({
-                    name : 'dropdown'
-                })).render(this.el.querySelector('.profile-select-options'));
+      if (Person.ownsOrganizations()) {
+        var currentEntityView = Person.get();
 
-                if (this.organization) {
-                    currentEntityView = this.organization;
-                }
+        this.appendChild(new CV.UI.FeedDropdown({
+          name : 'dropdown'
+        })).render(this.el.querySelector('.profile-select-options'));
 
-                this.dropdown.selectByEntity(currentEntityView);
-            }
-
-            this.appendChild(new CV.FeedSidebar({
-                name : 'sidebar',
-                el : this.el.querySelector('[data-feed-sidebar]')
-            }));
-
-            if (Person.anon()) {
-                this.appendChild(new CV.FeedAnonymousOnboarding({
-                    name: 'onboarding'
-                })).render(this.el.querySelector('.profile-body'));
-
-                this.sidebar.fetchTopVoices();
-
-                return;
-            }
-
-            this._updateFeed();
-            this.sidebar.fetchTopVoices().showStats();
-        },
-
-        _updateFeed : function _updateFeed() {
-            if (this.feedItems && this.feedItems.feed.length) {
-                return this._renderFeed();
-            }
-
-            this.appendChild(new CV.FeedOnboarding({
-                name: 'onboarding'
-            })).render(this.el.querySelector('.profile-body'));
-        },
-
-        _renderFeed : function _renderFeed() {
-            var feedList = document.createElement('div');
-            feedList.className = 'feed__list';
-
-            this.feedItems.feed.reverse().forEach(function(item, index) {
-                this.appendChild(CV.FeedItem.create({
-                    name : 'feed-item__' + index,
-                    className : 'cv-items-list',
-                    data : item
-                })).render(feedList).showDate();
-            }, this);
-
-            this.el.querySelector('.profile-body').appendChild(feedList);
+        if (this.organization) {
+          currentEntityView = this.organization;
         }
+
+        this.dropdown.selectByEntity(currentEntityView);
+      }
+
+      this.appendChild(new CV.FeedSidebar({
+        name : 'sidebar',
+        el : this.el.querySelector('[data-feed-sidebar]')
+      }));
+
+      if (Person.anon()) {
+        this.appendChild(new CV.FeedAnonymousOnboarding({
+          name: 'onboarding'
+        })).render(this.el.querySelector('.profile-body'));
+
+        this.sidebar.fetchTopVoices();
+
+        return;
+      }
+
+      this._updateFeed();
+      this.sidebar.fetchTopVoices().showStats();
+    },
+
+    _updateFeed: function _updateFeed() {
+      if (this.feedItems && this.feedItems.feed.length) {
+        return this._renderFeed();
+      }
+
+      this.appendChild(new CV.FeedOnboarding({
+        name: 'onboarding'
+      })).render(this.el.querySelector('.profile-body'));
+    },
+
+    _renderFeed: function _renderFeed() {
+      var feedList = document.createElement('div');
+      feedList.className = 'feed__list';
+
+      this.feedItems.feed.reverse().forEach(function(item, index) {
+        this.appendChild(CV.FeedItem.create({
+          name : 'feed-item__' + index,
+          className : 'cv-items-list',
+          data : item
+        })).render(feedList).showDate();
+      }, this);
+
+      this.el.querySelector('.profile-body').appendChild(feedList);
     }
+  }
 });
