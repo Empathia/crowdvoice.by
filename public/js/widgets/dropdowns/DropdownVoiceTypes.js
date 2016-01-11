c55246c9a915e99d2d3b90683e557c8c114acec6
Noel Delgado <auhcsei@gmail.com>
2016-01-10 20:36:15 -0600

    update / reindent DropdownVoiceTypes

--- public/js/widgets/dropdowns/DropdownVoiceTypes.js
+++ public/js/widgets/dropdowns/DropdownVoiceTypes.js
@@ -1,92 +1,106 @@
 var Events = require('./../../lib/events');
 
 Class(CV.UI, 'DropdownVoiceTypes').inherits(Widget)({
-    HTML : '\
-        <div class="ui-form-field">\
-            <label class="-block">\
-                <span class="ui-input__label -upper -font-bold">Voice Type</span>\
-            </label>\
-        </div>',
+  HTML: '\
+    <div class="ui-form-field">\
+      <label class="-block">\
+        <span class="ui-input__label -upper -font-bold">Voice Type</span>\
+      </label>\
+    </div>',
 
-    prototype : {
-        _items : null,
-        _value : null,
-        init : function init(config) {
-            Widget.prototype.init.call(this, config);
-            this.el = this.element[0];
+  prototype: {
+    _items: null,
+    _value: null,
 
-            this.appendChild(new CV.Dropdown({
-                name : 'dropdown',
-                label : '- Select the voice type',
-                showArrow : true,
-                className : 'dropdown-topics ui-dropdown-styled -lg',
-                arrowClassName : '-s10 -color-grey',
-                bodyClassName : 'ui-vertical-list hoverable -block',
-                content: '\
-                    <div class="ui-vertical-list-item" data-value="TYPE_PUBLIC">Public</div>\
-                    <div class="ui-vertical-list-item" data-value="TYPE_CLOSED">Closed</div>\
-                '
-            })).render(this.el);
+    init: function init(config) {
+      Widget.prototype.init.call(this, config);
+      this.el = this.element[0];
 
-            this._items = [].slice.call(this.dropdown.getContent());
+      this.appendChild(new CV.Dropdown({
+        name: 'dropdown',
+        label: '- Select the voice type',
+        showArrow: true,
+        className: 'dropdown-topics ui-dropdown-styled -lg',
+        arrowClassName: '-s10 -color-grey',
+        bodyClassName: 'ui-vertical-list hoverable -block',
+        content: '\
+          <div class="ui-vertical-list-item" data-value="TYPE_PUBLIC">Public</div>\
+          <div class="ui-vertical-list-item" data-value="TYPE_CLOSED">Closed</div>'
+      })).render(this.el);
 
-            this._bindEvents();
-        },
+      this._items = [].slice.call(this.dropdown.getContent());
 
-        _bindEvents : function _bindEvents() {
-            this._clickHandlerRef = this._clickHandler.bind(this);
-            this._items.forEach(function(item) {
-                Events.on(item, 'click', this._clickHandlerRef);
-            }, this);
-            return this;
-        },
+      this._bindEvents();
+    },
 
-        _clickHandler : function _clickHandler(ev) {
-            this.selectByElement(ev.target);
-        },
+    /* Subscribe widget’s events.
+     * @private
+     */
+    _bindEvents: function _bindEvents() {
+      this._clickHandlerRef = this._clickHandler.bind(this);
+      this._items.forEach(function(item) {
+        Events.on(item, 'click', this._clickHandlerRef);
+      }, this);
+      return this;
+    },
 
-        selectByElement : function selectByElement(element) {
-            this._value = element.dataset.value;
-            this._items.forEach(function(item) {
-                item.classList.remove('active');
-            });
-            element.classList.add('active');
-            this.dropdown.setLabel(element.textContent).deactivate();
-            return this;
-        },
+    /* Handle the dropdown items click.
+     * @private
+     */
+    _clickHandler: function _clickHandler(ev) {
+      this.selectByElement(ev.target);
+    },
 
-        selectByValue : function selectByValue(value) {
-            this._items.some(function(i) {
-                if (i.getAttribute('data-value') === value) {
-                    this.selectByElement(i);
-                    return true;
-                }
-            }, this);
-            return this;
-        },
+    /* Selects dropdown option by item element.
+     * @public
+     * @param {NodeElement} element - the dropdown option element to select.
+     */
+    selectByElement: function selectByElement(element) {
+      this._value = element.dataset.value;
+      this._items.forEach(function(item) {
+        item.classList.remove('active');
+      });
+      element.classList.add('active');
+      this.dropdown.setLabel(element.textContent).deactivate();
+      return this;
+    },
 
-        /* Returns the data-value of the current selected option
-         * @method getValue <public>
-         */
-        getValue : function getValue() {
-            return this._value;
-        },
-
-        /* Sets the error state on the dropdown.
-         * @method error <public>
-         */
-        error : function error() {
-            this.dropdown.error();
-            return this;
-        },
-
-        destroy : function destroy() {
-            Widget.prototype.destroy.call(this);
-            this._items.forEach(function(item) {
-                Events.off(item, 'click', this._clickHandlerRef);
-            }, this);
-            this._clickHandlerRef = null;
-            return null;
+    /* Selects dropdown option by item `data-value`.
+     * @public
+     * @param {string} value - the option’s data to be selected.
+     */
+    selectByValue: function selectByValue(value) {
+      this._items.some(function(i) {
+        if (i.getAttribute('data-value') === value) {
+          this.selectByElement(i);
+          return true;
         }
+      }, this);
+      return this;
+    },
+
+    /* Returns the `data-value` value of the current option selected.
+     * @public
+     */
+    getValue: function getValue() {
+      return this._value;
+    },
+
+    /* Sets the error state on the dropdown.
+     * @public
+     */
+    error: function error() {
+      this.dropdown.error();
+      return this;
+    },
+
+    destroy: function destroy() {
+      Widget.prototype.destroy.call(this);
+      this._items.forEach(function(item) {
+        Events.off(item, 'click', this._clickHandlerRef);
+      }, this);
+      this._clickHandlerRef = null;
+      return null;
     }
+  }
 });
