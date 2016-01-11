1fc82e290f4c451a095893f459887fd7992a0ae2
Noel Delgado <auhcsei@gmail.com>
2016-01-11 17:16:24 -0600

    update / reindent VoiceCoverActions

--- public/js/widgets/voice-cover/actions/VoiceCoverActions.js
+++ public/js/widgets/voice-cover/actions/VoiceCoverActions.js
@@ -1,24 +1,24 @@
 Class(CV, 'VoiceCoverActions').inherits(Widget).includes(CV.WidgetUtils)({
-    ELEMENT_CLASS : 'cv-cover-actions cv-button-group multiple',
-    prototype : {
-        voiceEntity : null,
-        init : function init(config) {
-            Widget.prototype.init.call(this, config);
-            this.el = this.element[0];
+  ELEMENT_CLASS: 'cv-cover-actions cv-button-group multiple',
+  prototype: {
+    voiceEntity: null,
+    init: function init(config) {
+      Widget.prototype.init.call(this, config);
+      this.el = this.element[0];
 
-            this.appendChild(new CV.VoiceCoverActionsEdit({
-                name : 'edit',
-                voiceEntity : this.voiceEntity,
-                data : {value: 'Edit'}
-            })).render(this.el);
+      this.appendChild(new CV.VoiceCoverActionsEdit({
+        name: 'edit',
+        voiceEntity: this.voiceEntity,
+        data: {value: 'Edit'}
+      })).render(this.el);
 
-            if (this.voiceEntity.status !== CV.VoiceView.STATUS_ARCHIVED) {
-                this.appendChild(new CV.VoiceCoverActionsArchive({
-                    name : 'archive',
-                    voiceEntity : this.voiceEntity,
-                    data : {value: 'Archive'}
-                })).render(this.el);
-            }
-        }
+      if (this.voiceEntity.status !== CV.VoiceView.STATUS_ARCHIVED) {
+        this.appendChild(new CV.VoiceCoverActionsArchive({
+          name: 'archive',
+          voiceEntity: this.voiceEntity,
+          data: {value: 'Archive'}
+        })).render(this.el);
+      }
     }
+  }
 });
