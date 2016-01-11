9c528f52d3895e2dbbf959b3fbd7dfd93ae9f2f2
Noel Delgado <auhcsei@gmail.com>
2016-01-11 17:12:55 -0600

    update / reindent MyVoices widgets

--- public/js/views/MyVoices.js
+++ public/js/views/MyVoices.js
@@ -1,61 +1,63 @@
 Class(CV.Views, 'MyVoices').includes(NodeSupport, CV.WidgetUtils)({
-    prototype : {
-        /* Voices separated by STATUS.
-         * @property voices <required> [Object]
-         */
-        voices: null,
-        voicesLength : 0,
+  prototype: {
+    /* Voices separated by STATUS.
+     * @property {Object} voices
+     */
+    voices: null,
+    voicesLength : 0,
 
-        init : function init(config) {
-            Object.keys(config || {}).forEach(function(propertyName) {
-                this[propertyName] = config[propertyName];
-            }, this);
+    init: function init(config) {
+      Object.keys(config || {}).forEach(function(propertyName) {
+        this[propertyName] = config[propertyName];
+      }, this);
 
-            this._setup();
-        },
+      this._setup();
+    },
 
-        _setup : function _setup() {
-            if (this.voicesLength) {
-                return this._setupTabs();
-            }
+    _setup: function _setup() {
+      if (this.voicesLength) {
+        return this._setupTabs();
+      }
 
-            return this._setupOnboardingMessage();
-        },
+      return this._setupOnboardingMessage();
+    },
 
-        _setupOnboardingMessage : function _setupOnboardingMessage() {
-            while(this.el.firstChild) {
-                this.el.removeChild(this.el.firstChild);
-            }
+    _setupOnboardingMessage: function _setupOnboardingMessage() {
+      while(this.el.firstChild) {
+        this.el.removeChild(this.el.firstChild);
+      }
 
-            this.appendChild(new CV.MyVoicesOnboarding({
-                name : 'onboarding'
-            })).render(this.el);
-        },
+      this.appendChild(new CV.MyVoicesOnboarding({
+        name: 'onboarding'
+      })).render(this.el);
+    },
 
-        _setupTabs : function _setupTabs() {
-            this.appendChild(new CV.TabsManager({
-                name : 'tabs',
-                useHash : true,
-                nav : document.querySelector('.profile-menu'),
-                content : document.querySelector('.profile-menu-content')
-            }));
+    _setupTabs: function _setupTabs() {
+      this.appendChild(new CV.TabsManager({
+        name: 'tabs',
+        useHash: true,
+        nav: document.querySelector('.profile-menu'),
+        content: document.querySelector('.profile-menu-content')
+      }));
 
-            Object.keys(this.voices).forEach(function(propertyName) {
-                var tab = {};
-                tab.name = propertyName;
-                tab.content = CV.MyVoicesTab;
-                tab.contentData = {
-                    name : propertyName,
-                    voices : this.voices[propertyName]
-                };
-                tab.title = this.format.capitalizeFirstLetter(propertyName);
-                if (tab.contentData.voices.length) {
-                    tab.title += ' (' + tab.contentData.voices.length + ')';
-                }
-                this.tabs.addTab(tab);
-            }, this);
+      Object.keys(this.voices).forEach(function (propertyName) {
+        var tab = {};
+        tab.name = propertyName;
+        tab.content = CV.MyVoicesTab;
+        tab.contentData = {
+          name: propertyName,
+          voices: this.voices[propertyName]
+        };
+        tab.title = this.format.capitalizeFirstLetter(propertyName);
 
-            this.tabs.addTabIndicator().start();
+        if (tab.contentData.voices.length) {
+          tab.title += ' (' + tab.contentData.voices.length + ')';
         }
+
+        this.tabs.addTab(tab);
+      }, this);
+
+      this.tabs.addTabIndicator().start();
     }
+  }
 });
