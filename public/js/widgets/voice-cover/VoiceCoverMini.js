6d9cf9ae7e8fe3b8b4f5471be5856c0dbbdcd9ea
Noel Delgado <auhcsei@gmail.com>
2016-01-10 22:42:35 -0600

    add / voice-cover mini {open,closed} badges

--- public/js/widgets/voice-cover/VoiceCoverMini.js
+++ public/js/widgets/voice-cover/VoiceCoverMini.js
@@ -1,118 +1,137 @@
 var Person = require('./../../lib/currentPerson');
 
 Class(CV, 'VoiceCoverMini').inherits(Widget).includes(CV.WidgetUtils)({
-    HTML : '\
-        <article class="cv-voice-cover mini -clearfix" role="article">\
-            <a href="#" class="-float-left" data-voice-anchor>\
-                <img class="voice-cover" width="36" height="36"/>\
+  HTML: '\
+    <article class="cv-voice-cover mini -clearfix" role="article">\
+      <a href="#" class="-float-left" data-voice-anchor>\
+        <img class="voice-cover" width="36" height="36"/>\
+      </a>\
+      <div class="voice-content">\
+        <p class="voice-cover-title-wrapper">\
+          <a href="#" class="voice-cover-title voice-cover-title-anchor -font-semi-bold -tdn" data-voice-anchor>{{voice-title}}</a>\
+        </p>\
+        <div class="meta">\
+          <div class="author -inline-block">\
+            By <a class="author-anchor" href="{{voice-owner-url}}">\
+            <span class="author-username">{{voice-owner-name}}</span>\
             </a>\
-            <div class="voice-content">\
-                <a href="#" class="voice-cover-title voice-cover-title-anchor -font-semi-bold -tdn" data-voice-anchor>\
-                    {{voice-title}}\
-                </a>\
-                <div class="meta">\
-                    <div class="author -inline-block">\
-                        By <a class="author-anchor" href="{{voice-owner-url}}">\
-                        <span class="author-username">{{voice-owner-name}}</span>\
-                        </a>\
-                    </div>\
-                </div>\
-            </div>\
-            <div class="action -abs"></div>\
-        </article>',
+          </div>\
+        </div>\
+      </div>\
+      <div class="action -abs"></div>\
+    </article>',
 
-    TOPICS_HTML : ' · <ul class="cv-tags -inline-block -list-horizontal"></ul>',
+  TOPICS_HTML: ' · <ul class="cv-tags -inline-block -list-horizontal"></ul>',
 
-    TOPIC_ITEM_HTML : '\
-        <li class="cv-tags-list-item">\
-            <a class="cv-tags-tag" href="{{tag-url}}">{{tag-name}}</a>\
-        </li>',
+  TOPIC_ITEM_HTML: '\
+    <li class="cv-tags-list-item">\
+      <a class="cv-tags-tag" href="{{tag-url}}">{{tag-name}}</a>\
+    </li>',
 
-    prototype : {
-        /* VoiceEntity
-         * @property data <required> [Object]
-         */
-        data : {},
+  TEMPLATE: '<span class="badge-follows voice-cover-type-badge">{TYPE}</span>',
 
-        init : function init(config) {
-            Widget.prototype.init.call(this, config);
+  prototype: {
+    /* VoiceEntity
+     * @property {Object} data
+     */
+    data: {},
 
-            this.el = this.element[0];
-            this.authorAnchor = this.el.querySelector('.author-anchor');
-            this.voiceAnchors = [].slice.call(this.el.querySelectorAll('[data-voice-anchor]'), 0);
-            this.actionsElement = this.el.querySelector('.action');
+    init: function init(config) {
+      Widget.prototype.init.call(this, config);
 
-            this._setup();
-        },
+      this.el = this.element[0];
+      this.authorAnchor = this.el.querySelector('.author-anchor');
+      this.voiceAnchors = [].slice.call(this.el.querySelectorAll('[data-voice-anchor]'), 0);
+      this.actionsElement = this.el.querySelector('.action');
 
-        addActions : function addActions() {
-            if (!Person.get()) {
-                return;
-            }
+      this._setup();
+    },
 
-            if (Person.ownerOf('voice', this.data.id)) {
-                this.appendChild(new CV.VoiceCoverActions({
-                    name : 'actions',
-                    voiceEntity : this.data
-                })).render(this.actionsElement);
-            }
-        },
+    /* @private
+     */
+    _setup: function _setup() {
+      window.CardHoverWidget.register(this.authorAnchor, this.data.owner);
 
-        _setup : function _setup() {
-            window.CardHoverWidget.register(this.authorAnchor, this.data.owner);
+      this._addTypeBadge();
 
-            this.voiceAnchors.forEach(function(anchor) {
-                this.dom.updateAttr('href', anchor, '/' + this.data.owner.profileName + '/' + this.data.slug + '/');
-                this.dom.updateAttr('title', anchor, this.data.title + ' voice');
-            }, this);
+      this.voiceAnchors.forEach(function(anchor) {
+        this.dom.updateAttr('href', anchor, '/' + this.data.owner.profileName + '/' + this.data.slug + '/');
+        this.dom.updateAttr('title', anchor, this.data.title + ' voice');
+      }, this);
 
-            if (this.data.images.small) {
-                this.dom.updateAttr('src', this.el.querySelector('.voice-cover'), this.data.images.small.url);
-            } else {
-                this.el.querySelector('.voice-cover').classList.add('-colored-background');
-            }
-            this.dom.updateText(this.el.querySelector('.voice-cover-title'), this.data.title);
+      if (this.data.images.small) {
+        this.dom.updateAttr('src', this.el.querySelector('.voice-cover'), this.data.images.small.url);
+      } else {
+        this.el.querySelector('.voice-cover').classList.add('-colored-background');
+      }
+      this.dom.updateText(this.el.querySelector('.voice-cover-title'), this.data.title);
 
-            this.dom.updateAttr('href', this.authorAnchor, '/' + this.data.owner.profileName + '/');
-            this.dom.updateAttr('title', this.authorAnchor, this.data.owner.profileName + ' profile');
+      this.dom.updateAttr('href', this.authorAnchor, '/' + this.data.owner.profileName + '/');
+      this.dom.updateAttr('title', this.authorAnchor, this.data.owner.profileName + ' profile');
 
-            this.dom.updateText(this.el.querySelector('.author-username'), this.data.owner.name);
-        },
+      this.dom.updateText(this.el.querySelector('.author-username'), this.data.owner.name);
+    },
 
-        /* Show the location, joined at and inline name, username
-         * @method showMeta <public> [Function]
-         */
-        showMeta : function showMeta() {
-            this.el.querySelector('.meta').insertAdjacentHTML('beforeend', this.constructor.TOPICS_HTML);
+    /* Add the voice cover actions for currentPerson’s voices.
+     * @public
+     */
+    addActions: function addActions() {
+      if (!Person.get()) return;
 
-            this._createTags(this.data.topics);
-        },
+      if (Person.ownerOf('voice', this.data.id)) {
+        this.appendChild(new CV.VoiceCoverActions({
+          name: 'actions',
+          voiceEntity: this.data
+        })).render(this.actionsElement);
+      }
+    },
 
-        /**
-         * Creates a tag per topic that is tagged to the topic and appends them.
-         * @method _createTags <private> [Function]
-         * @params topics <required> [Array] list of topics tagged to the voice
-         * @return CV.VoiceCoverMini
-         */
-        _createTags : function _createTags(topics) {
-            this.tagListElement = this.el.querySelector('.cv-tags');
+    /* Show the location, joined at and inline name, username
+     * @public
+     */
+    showMeta: function showMeta() {
+      this.el.querySelector('.meta').insertAdjacentHTML('beforeend', this.constructor.TOPICS_HTML);
+      this._createTags(this.data.topics);
+    },
 
-            topics.forEach(function(topic) {
-                this.tagListElement.insertAdjacentHTML('beforeend', this.constructor.TOPIC_ITEM_HTML);
-                var anchors = this.tagListElement.getElementsByClassName('cv-tags-tag');
-                var anchor = anchors[anchors.length-1];
+    _addTypeBadge: function _addTypeBadge() {
+      var text = '';
+      switch(this.data.type) {
+        case CV.VoiceView.TYPE_PUBLIC:
+          text = 'OPEN';
+          break;
+        case CV.VoiceView.TYPE_CLOSED:
+          text = 'CLOSED';
+          break;
+      }
+      this.el.querySelector('.voice-cover-title-wrapper').insertAdjacentHTML('beforeend', this.constructor.TEMPLATE.replace(/{TYPE}/, text));
+    },
 
-                this.dom.updateText(anchor, topic.name);
-                this.dom.updateAttr('href', anchor, '/topic/' + topic.slug);
-            }, this);
+    /**
+     * Creates a tag per topic that is tagged to the topic and appends them.
+     * @private
+     * @params {Array} topics - list of topics tagged to the voice
+     * @return {Object} CV.VoiceCoverMini
+     */
+    _createTags: function _createTags(topics) {
+      this.tagListElement = this.el.querySelector('.cv-tags');
 
-           return this;
-        },
+      topics.forEach(function(topic) {
+        this.tagListElement.insertAdjacentHTML('beforeend', this.constructor.TOPIC_ITEM_HTML);
+        var anchors = this.tagListElement.getElementsByClassName('cv-tags-tag');
+        var anchor = anchors[anchors.length-1];
 
-        destroy : function destroy() {
-            window.CardHoverWidget.unregister(this.authorAnchor);
-            Widget.prototype.destroy.call(this);
-            return null;
-        }
+        this.dom.updateText(anchor, topic.name);
+        this.dom.updateAttr('href', anchor, '/topic/' + topic.slug);
+      }, this);
+
+      return this;
+    },
+
+    destroy: function destroy() {
+      window.CardHoverWidget.unregister(this.authorAnchor);
+      Widget.prototype.destroy.call(this);
+      return null;
     }
+  }
 });
