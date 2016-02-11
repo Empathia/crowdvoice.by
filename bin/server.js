a02e6bacf82a00ff406cba4281efcdf7f4762a65
greduan <me@greduan.com>
2016-02-11 13:21:02 -0600

    Add K.EntitiesPresenter

--- bin/server.js
+++ bin/server.js
@@ -2,21 +2,19 @@
 
 var application = require('neonode-core');
 
-// Load socket.io
 var io = require('socket.io')(application.server);
 
 global.FeedInjector = require(__dirname + '/../lib/FeedInjector.js');
 
 require(path.join(process.cwd(), 'lib', 'krypton', 'load-models.js'));
 
-// Load routes
-require('./../lib/routes.js');
+require(path.join(process.cwd(), 'lib', 'routes.js'));
 
-require('./../lib/TwitterFetcher');
+require(path.join(process.cwd(), 'lib', 'TwitterFetcher.js'));
+
+require(path.join(process.cwd(), 'presenters', 'PostsPresenter.js'));
 
 application._serverStart();
-
-require('./../presenters/PostsPresenter');
 
 io.use(function(socket, next) {
   sessionMiddleWare(socket.request, socket.request.res, next);
