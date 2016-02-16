96df9a2d296af0d04c2d0a89d256f33d1acb902a
Noel Delgado <auhcsei@gmail.com>
2016-02-16 11:02:50 -0600

    add / FeedItem `getLink` abstract methods

--- public/js/widgets/feed/FeedHasInvitedYouToBecomeAContributor.js
+++ public/js/widgets/feed/FeedHasInvitedYouToBecomeAContributor.js
@@ -19,6 +19,13 @@ Class(CV, 'FeedHasInvitedYouToBecomeAContributor').inherits(CV.FeedItem)({
         name: 'voice-cover',
         data: this.data.voice
       })).render(this.extraInfoElement);
+    },
+
+    /* Creates the notification url that makes more sense to redirect when clicked.
+     * @abstract
+     */
+    getLink: function getLink() {
+      return this.getVoiceUrl();
     }
   }
 });
