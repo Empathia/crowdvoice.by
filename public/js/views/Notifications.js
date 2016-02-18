ec565afe7c5320f1b6c7b96656cf0dff9113ffea
Noel Delgado <auhcsei@gmail.com>
2016-02-18 13:03:10 -0600

    add / NotificationsPageItem, mark as read action

--- public/js/views/Notifications.js
+++ public/js/views/Notifications.js
@@ -30,8 +30,11 @@ Class(CV.Views, 'Notifications').includes(NodeSupport, CustomEventSupport)({
       this._notificationsHandlerRef = this._notificationsHandler.bind(this);
       NotificationsStore.bind('notifications', this._notificationsHandlerRef);
 
+      this._notificationMarkAsReadAndRedirectHandlerRef = this._notificationMarkAsReadAndRedirectHandler.bind(this);
+      this.bind('notification:markAsReadAndRedirect', this._notificationMarkAsReadAndRedirectHandlerRef);
+
       this._notificationMarkAsReadHandlerRef = this._notificationMarkAsReadHandler.bind(this);
-      this.bind('notification:markAsReadAndRedirect', this._notificationMarkAsReadHandlerRef);
+      this.bind('notification:markAsRead', this._notificationMarkAsReadHandlerRef);
 
       this._scrollHandlerRef = this._scrollHandler.bind(this);
       Events.on(this._window, 'scroll', this._scrollHandlerRef);
@@ -64,21 +67,36 @@ Class(CV.Views, 'Notifications').includes(NodeSupport, CustomEventSupport)({
       this.profileBody.appendChild(fragment);
     },
 
+    /* NotificationItem 'notification:markAsReadAndRedirect' event handler.
+     * @private
+     */
+    _notificationMarkAsReadAndRedirectHandler: function _notificationMarkAsReadAndRedirectHandler(ev) {
+      ev.stopPropagation();
+      NotificationsStore.decreaseUnseen();
+      this._markAsRead(ev.target.notificationId, function (err, res) {
+        if (err) return console.log(res);
+        if (ev.redirectUrl) window.location = ev.redirectUrl;
+      });
+    },
+
     /* NotificationItem 'notification:markAsRead' event handler.
      * @private
      */
     _notificationMarkAsReadHandler: function _notificationMarkAsReadHandler(ev) {
       ev.stopPropagation();
       NotificationsStore.decreaseUnseen();
+      this._markAsRead(ev.target.notificationId, function (err, res) {
+        if (err) return console.log(res);
+        NotificationsStore.reFetchNotifications();
+      });
+    },
 
+    _markAsRead: function _markAsRead(notificationId, callback) {
       API.markNotificationAsRead({
         profileName: Person.get('profileName'),
-        data: {notificationId: ev.target.notificationId}
+        data: {notificationId: notificationId}
       }, function(err, res) {
-        if (err) return console.log(res);
-        if (ev.redirectUrl) {
-          window.location = ev.redirectUrl;
-        }
+        callback(err, res);
       });
     },
 
--- public/js/widgets/view-notifications/NotificationsPageItem.js
+++ public/js/widgets/view-notifications/NotificationsPageItem.js
@@ -15,7 +15,7 @@ Class(CV, 'NotificationsPageItem').inherits(Widget).includes(BubblingSupport)({
      * @property {date} data.updatedAt
      */
     data: null,
-
+    markAsReadElement: null,
     init: function init(config) {
       Widget.prototype.init.call(this, config);
       this.el = this.element[0];
@@ -53,9 +53,26 @@ Class(CV, 'NotificationsPageItem').inherits(Widget).includes(BubblingSupport)({
       }
     },
 
+    _markAsReadHandler: function _markAsReadHandler() {
+      this._setReadState();
+      this.dispatch('notification:markAsRead');
+      return false;
+    },
+
     _setUnreadState: function _setUnreadState() {
       this.element.addClass('-is-unread');
       this.el.insertAdjacentHTML('beforeend', this.constructor.TEMPLATE_MARK_AS_READ);
+      this.markAsReadElement = this.el.querySelector('.cv-notification-page-item-mark-as-read');
+      this._markAsReadHandlerRef = this._markAsReadHandler.bind(this);
+      Events.on(this.markAsReadElement, 'click', this._markAsReadHandlerRef);
+    },
+
+    _setReadState: function _setReadState() {
+      this.element.removeClass('-is-unread');
+      Events.off(this.markAsReadElement, 'click', this._markAsReadHandlerRef);
+      this._markAsReadHandlerRef = null;
+      this.markAsReadElement.parentNode.removeChild(this.markAsReadElement);
+      this.markAsReadElement = null;
     },
 
     destroy: function destroy() {
