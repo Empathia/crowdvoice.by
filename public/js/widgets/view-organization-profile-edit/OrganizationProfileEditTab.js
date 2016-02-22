0c134846daaf21e5afe87e700ac77f1b25bdd702
Noel Delgado <auhcsei@gmail.com>
2016-02-22 17:25:20 -0600

    fix / 75, Editing profile name causes 404

--- public/js/widgets/view-organization-profile-edit/OrganizationProfileEditTab.js
+++ public/js/widgets/view-organization-profile-edit/OrganizationProfileEditTab.js
@@ -286,9 +286,10 @@ Class(CV, 'OrganizationProfileEditTab').inherits(Widget)({
                 this.profilenameInput.clearState().updateHint();
 
                 if (res.status === 'success') {
+                    var url = '/' + this.profilenameInput.getValue() + '/edit/#profile';
                     this._displaySuccessAlert('Your profile has been updated.');
                     setTimeout(function() {
-                        window.location.reload();
+                      window.location.replace(url);
                     }, 1000);
                 }
             }.bind(this));
