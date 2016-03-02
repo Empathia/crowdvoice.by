55958362a892c6ff2661665d4f6b1b2012a19d68
greduan <me@greduan.com>
2016-03-01 18:44:47 -0600

    Add repost_of_id column to Posts table

--- models/Post.js
+++ models/Post.js
@@ -165,6 +165,7 @@ var Post = Class('Post').inherits(Argon.KnexModel).includes(ImageUploader)({
     createdAt     : null,
     updatedAt     : null,
     extras        : {},
+    repostOfId    : null,
 
     init : function init(config) {
       Argon.KnexModel.prototype.init.call(this, config);
